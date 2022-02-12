6ab4a73f08caa993431fc852e2e6859dfccba7e8
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-02-12 16:16:40 +1000

    Draft model porous

--- src/model_porous.rs
+++ src/model_porous.rs
@@ -1,14 +1,21 @@
-use crate::{ModelConductivity, ModelLiquidRetention, ModelRealDensity, ParamSeepageLiq, ParamSeepageLiqGas, StrError};
+use crate::{
+    ModelConductivity, ModelLiquidRetention, ModelRealDensity, ModelStressStrain, ParamPorousSolLiq,
+    ParamPorousSolLiqGas, StrError,
+};
 
-pub struct ModelSeepageLiq {
+pub struct ModelPorousSolLiq {
     pub porosity_initial: f64,
+    pub density_solid: f64,
+    pub model_stress_strain: ModelStressStrain,
     pub model_density_liquid: ModelRealDensity,
     pub model_conductivity_liquid: ModelConductivity,
     pub model_retention_liquid: ModelLiquidRetention,
 }
 
-pub struct ModelSeepageLiqGas {
+pub struct ModelPorousSolLiqGas {
     pub porosity_initial: f64,
+    pub density_solid: f64,
+    pub model_stress_strain: ModelStressStrain,
     pub model_density_liquid: ModelRealDensity,
     pub model_density_gas: ModelRealDensity,
     pub model_conductivity_liquid: ModelConductivity,
@@ -16,10 +23,12 @@ pub struct ModelSeepageLiqGas {
     pub model_retention_liquid: ModelLiquidRetention,
 }
 
-impl ModelSeepageLiq {
-    pub fn new(params: &ParamSeepageLiq, two_dim: bool) -> Result<Self, StrError> {
-        let model = ModelSeepageLiq {
+impl ModelPorousSolLiq {
+    pub fn new(params: &ParamPorousSolLiq, two_dim: bool, plane_stress: bool) -> Result<Self, StrError> {
+        let model = ModelPorousSolLiq {
             porosity_initial: params.porosity_initial,
+            density_solid: params.density_solid,
+            model_stress_strain: ModelStressStrain::new(&params.stress_strain, two_dim, plane_stress)?,
             model_density_liquid: ModelRealDensity::new(&params.density_liquid)?,
             model_conductivity_liquid: ModelConductivity::new(&params.conductivity_liquid, two_dim)?,
             model_retention_liquid: ModelLiquidRetention::new(&params.retention_liquid)?,
@@ -28,10 +37,12 @@ impl ModelSeepageLiq {
     }
 }
 
-impl ModelSeepageLiqGas {
-    pub fn new(params: &ParamSeepageLiqGas, two_dim: bool) -> Result<Self, StrError> {
-        let model = ModelSeepageLiqGas {
+impl ModelPorousSolLiqGas {
+    pub fn new(params: &ParamPorousSolLiqGas, two_dim: bool, plane_stress: bool) -> Result<Self, StrError> {
+        let model = ModelPorousSolLiqGas {
             porosity_initial: params.porosity_initial,
+            density_solid: params.density_solid,
+            model_stress_strain: ModelStressStrain::new(&params.stress_strain, two_dim, plane_stress)?,
             model_density_liquid: ModelRealDensity::new(&params.density_liquid)?,
             model_density_gas: ModelRealDensity::new(&params.density_gas)?,
             model_conductivity_liquid: ModelConductivity::new(&params.conductivity_liquid, two_dim)?,
