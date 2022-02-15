74999a0e7dbc1c9fce7a648585e884e52c6070f7
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-02-16 00:33:27 +1000

    Impl find layers

--- src/geostatics.rs
+++ src/geostatics.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code, unused_mut, unused_variables, unused_imports)]
 
-use crate::{ModelRealDensity, SimConfig, StrError};
+use crate::{ElementConfig, ModelRealDensity, SimConfig, StrError};
 use gemlab::mesh::{At, CellAttributeId, CellId, PointId};
 use russell_tensor::Tensor2;
 use std::collections::{HashMap, HashSet};
@@ -89,11 +89,80 @@ impl PorousLayer {
     }
 }
 
-/// Implements geostatic stress state calculator
-pub struct Geostatics {
-    // Access to configuration
-// config: &'a SimConfig<'a>,
+type MinMaxElevation = (f64, f64);
+
+type LayerLimits = (CellAttributeId, MinMaxElevation);
+
+fn find_layers_top_to_bottom(config: &SimConfig) -> Result<Vec<LayerLimits>, StrError> {
+    // mesh and space_ndim
+    let mesh = config.mesh;
+    let space_ndim = mesh.space_ndim;
+
+    // find cells near x_min
+    let mut cells_near_x_min: HashSet<CellId> = HashSet::new();
+    if mesh.space_ndim == 2 {
+        let edge_keys = mesh.find_boundary_edges(At::X(mesh.min[0]))?;
+        if edge_keys.len() < 1 {
+            return Err("cannot find at least one vertical edge at x_min");
+        }
+        for edge_key in &edge_keys {
+            let edge = mesh.boundary_edges.get(edge_key).unwrap();
+            for cell_id in &edge.shared_by_2d_cells {
+                cells_near_x_min.insert(*cell_id);
+            }
+        }
+    } else {
+        let face_keys = mesh.find_boundary_faces(At::X(mesh.min[0]))?;
+        if face_keys.len() < 1 {
+            return Err("cannot find at least one vertical face at x_min");
+        }
+        for face_key in &face_keys {
+            let face = mesh.boundary_faces.get(face_key).unwrap();
+            for cell_id in &face.shared_by_cells {
+                cells_near_x_min.insert(*cell_id);
+            }
+        }
+    }
+
+    // find layers (unsorted)
+    let mut layers: HashMap<CellAttributeId, MinMaxElevation> = HashMap::new();
+    for cell_id in &cells_near_x_min {
+        let cell = &mesh.cells[*cell_id];
+        let attribute_id = cell.attribute_id;
+        let element_config = config
+            .element_configs
+            .get(&attribute_id)
+            .ok_or("cannot find CellAttributeId in SimConfig")?;
+        match &element_config {
+            ElementConfig::PorousSolLiq(..) | ElementConfig::PorousSolLiqGas(..) => (),
+            _ => continue, // skip other elements
+        }
+        let shape = mesh.alloc_shape_cell(*cell_id)?;
+        match layers.get_mut(&attribute_id) {
+            Some(limits) => {
+                limits.0 = f64::min(limits.0, shape.min_coords[space_ndim - 1]);
+                limits.1 = f64::max(limits.1, shape.max_coords[space_ndim - 1]);
+            }
+            None => {
+                layers.insert(
+                    attribute_id,
+                    (shape.min_coords[space_ndim - 1], shape.max_coords[space_ndim - 1]),
+                );
+            }
+        };
+    }
+
+    // convert map to vec
+    let mut layers_top_to_bottom: Vec<_> = layers.keys().map(|k| (*k, *layers.get(k).unwrap())).collect();
+
+    // sort layers top-to-bottom
+    layers_top_to_bottom.sort_by(|(_, a_limits), (_, b_limits)| b_limits.0.partial_cmp(&a_limits.0).unwrap());
+    Ok(layers_top_to_bottom)
 }
+
+/// Implements geostatic stress state calculator
+#[derive(Debug)]
+pub struct Geostatics {}
 
 impl Geostatics {
     /// Returns a new StateGeostatic instance
@@ -112,62 +181,14 @@ impl Geostatics {
             return Err("geostatics initialization requires space_ndim = 2 or 3");
         }
 
-        // find cells near x_min
-        let mut cells_near_x_min: HashSet<CellId> = HashSet::new();
-        if space_ndim == 2 {
-            let edge_keys = mesh.find_boundary_edges(At::X(mesh.min[0]))?;
-            if edge_keys.len() < 1 {
-                return Err("cannot find at least one vertical edge at x_min");
-            }
-            for edge_key in &edge_keys {
-                let edge = mesh.boundary_edges.get(edge_key).unwrap();
-                for cell_id in &edge.shared_by_2d_cells {
-                    cells_near_x_min.insert(*cell_id);
-                }
-            }
-        } else {
-            let face_keys = mesh.find_boundary_faces(At::X(mesh.min[0]))?;
-            if face_keys.len() < 1 {
-                return Err("cannot find at least one vertical face at x_min");
-            }
-            for face_key in &face_keys {
-                let face = mesh.boundary_faces.get(face_key).unwrap();
-                for cell_id in &face.shared_by_cells {
-                    cells_near_x_min.insert(*cell_id);
-                }
-            }
-        }
+        // compute overburden stress
+        // let mut cumulated_overburden = 0.0;
+        // for (attribute_id, limits) in layers_top_to_bottom {
+        //     let element_config = config.element_configs.get(attribute_id).unwrap();
+        // }
 
-        // find layers
-        #[derive(Debug)]
-        struct MinMaxElevation(f64, f64);
-        let mut layers: HashMap<CellAttributeId, MinMaxElevation> = HashMap::new();
-        for cell_id in &cells_near_x_min {
-            let cell = &mesh.cells[*cell_id];
-            let attribute_id = cell.attribute_id;
-            println!(">>>>>>>>>>>. {}", attribute_id);
-            let element_config = config
-                .element_configs
-                .get(&attribute_id)
-                .ok_or("cannot find CellAttributeId in SimConfig")?;
-            let shape = mesh.alloc_shape_cell(*cell_id)?;
-            match layers.get_mut(&attribute_id) {
-                Some(limits) => {
-                    limits.0 = f64::min(limits.0, shape.min_coords[space_ndim - 1]);
-                    limits.1 = f64::max(limits.1, shape.max_coords[space_ndim - 1]);
-                }
-                None => {
-                    layers.insert(
-                        attribute_id,
-                        MinMaxElevation(shape.min_coords[space_ndim - 1], shape.max_coords[space_ndim - 1]),
-                    );
-                }
-            };
-        }
+        let layers_top_to_bottom = find_layers_top_to_bottom(config)?;
 
-        println!("{:?}", layers);
-
-        // Ok(Geostatics { config })
         Ok(Geostatics {})
     }
 
@@ -187,43 +208,49 @@ impl Geostatics {
 
 #[cfg(test)]
 mod tests {
-    use super::Geostatics;
+    use super::{find_layers_top_to_bottom, Geostatics};
     use crate::{ElementConfig, SampleParams, SimConfig, StrError};
     use gemlab::mesh::Mesh;
 
     #[test]
-    fn new_works() -> Result<(), StrError> {
+    fn find_layers_top_to_bottom_works() -> Result<(), StrError> {
         //
+        // 3.1  14---------15
+        //       |  [11]333 |
         // 3.0  10---------11---------12--------------------13
         //       |        .' '.        |                     |
         //       | [8]  .'     '.  [9] |                     |
-        //       | (2).'         '.(2) |        [10]         |  L
-        //       |  .'             '.  |         (2)         |  A
+        //       | 222.'         '.222 |        [10]         |  L
+        //       |  .'             '.  |         222         |  A
         //       |.'                 '.|                     |  Y
         // 2.0   7         [5]         8---------------------9  E
-        //       |'.       (2)       .'|                     |  R
-        //       |  '.             .'  |                     |  2
+        //       |'.       222       .'|                     |  R
+        //       |  '.             .'  |                     | 222
         //       | [4]'.         .'[6] |         [7]         |
-        //       | (2)  '.     .'  (2) |         (2)         |
+        //       | 222  '.     .'  222 |         222         |
         //       |        '. .'        |                     |
         // 1.0   3----------4----------5---------------------6  <-- layer separation
         //       |        .' '.        |                     |  L
         //       | [0]  .'     '.  [2] |                     |  A
-        //       | (1).'   [1]   '.(1) |         [3]         |  Y
-        //       |  .'     (1)     '.  |         (1)         |  E
+        //       | 111.'   [1]   '.111 |         [3]         |  Y
+        //       |  .'     111     '.  |         111         |  E
         //       |.'                 '.|                     |  R
-        // 0.0   0---------------------1---------------------2  1
+        // 0.0   0---------------------1---------------------2 111
         //
         //      0.0        1.0        2.0                   4.0
         //
         let mut mesh = Mesh::from_text_file("./data/meshes/rectangle_tris_quads.msh")?;
         let mut config = SimConfig::new(&mesh);
-        let params_1 = SampleParams::params_porous_sol_liq_gas(0.2, 1e-2);
-        let params_2 = SampleParams::params_porous_sol_liq_gas(0.4, 1e-2);
-        config.elements(1, ElementConfig::PorousSolLiqGas(params_1, None))?;
-        config.elements(2, ElementConfig::PorousSolLiqGas(params_2, None))?;
+        let p111 = SampleParams::params_porous_sol_liq_gas(0.2, 1e-2);
+        let p222 = SampleParams::params_porous_sol_liq_gas(0.4, 1e-2);
+        let p333 = SampleParams::params_solid();
+        config.elements(111, ElementConfig::PorousSolLiqGas(p111, None))?;
+        config.elements(222, ElementConfig::PorousSolLiqGas(p222, None))?;
+        config.elements(333, ElementConfig::Solid(p333, None))?;
         config.set_gravity(10.0)?; // m/s²
-        Geostatics::new(&config)?;
+        let layers = find_layers_top_to_bottom(&config)?;
+        assert_eq!(layers[0], (222, (1.0, 3.0)));
+        assert_eq!(layers[1], (111, (0.0, 1.0)));
         Ok(())
     }
 }
