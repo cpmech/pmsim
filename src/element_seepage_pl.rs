b7b82967376c657093d3d0062a6e0dcfa1578030
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-02-22 12:18:49 +1000

    [Important] Remove ModelSeepage

--- src/element_seepage_pl.rs
+++ src/element_seepage_pl.rs
@@ -1,28 +1,21 @@
-#![allow(dead_code, unused_mut, unused_variables)]
-
-use crate::{Element, EquationNumbers, ModelSeepage, ParamSeepage, SimStateInitializer, StateElement, StrError};
+use crate::{Element, EquationNumbers, ParamSeepage, SimStateInitializer, StateElement, StrError};
 use gemlab::shapes::Shape;
 
 /// Implements the pl (liquid pressure) element for seepage simulations
 pub struct ElementSeepagePl {
-    shape: Shape,
-    model: ModelSeepage, // material model
+    _shape: Shape,
 }
 
 impl ElementSeepagePl {
     /// Allocates a new instance
-    pub fn new(shape: Shape, params: &ParamSeepage, n_integ_point: Option<usize>) -> Result<Self, StrError> {
-        let two_dim = shape.space_ndim == 2;
-        Ok(ElementSeepagePl {
-            shape,
-            model: ModelSeepage::new(params, two_dim)?,
-        })
+    pub fn new(shape: Shape, _param: &ParamSeepage, _n_integ_point: Option<usize>) -> Result<Self, StrError> {
+        Ok(ElementSeepagePl { _shape: shape })
     }
 }
 
 impl Element for ElementSeepagePl {
     /// Activates an equation number, if not set yet
-    fn set_equation_numbers(&self, equation_numbers: &mut EquationNumbers) -> usize {
+    fn set_equation_numbers(&self, _equation_numbers: &mut EquationNumbers) -> usize {
         0
     }
 
@@ -37,17 +30,17 @@ impl Element for ElementSeepagePl {
     }
 
     /// Computes the element K-matrix
-    fn calc_local_kk_matrix(&mut self, first_iteration: bool) -> Result<(), StrError> {
+    fn calc_local_kk_matrix(&mut self, _first_iteration: bool) -> Result<(), StrError> {
         Ok(())
     }
 
     /// Assembles the local Y-vector into the global Y-vector
-    fn assemble_yy_vector(&self, yy: &mut Vec<f64>) -> Result<(), StrError> {
+    fn assemble_yy_vector(&self, _yy: &mut Vec<f64>) -> Result<(), StrError> {
         Ok(())
     }
 
     /// Assembles the local K-matrix into the global K-matrix
-    fn assemble_kk_matrix(&self, kk: &mut Vec<Vec<f64>>) -> Result<(), StrError> {
+    fn assemble_kk_matrix(&self, _kk: &mut Vec<Vec<f64>>) -> Result<(), StrError> {
         Ok(())
     }
 }
