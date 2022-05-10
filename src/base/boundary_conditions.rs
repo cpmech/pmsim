e59a030fe9700b14cef59ba4e92571d9adb810dc
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-05-11 09:09:47 +1000

    Add test

--- src/base/boundary_conditions.rs
+++ src/base/boundary_conditions.rs
@@ -26,3 +26,27 @@ pub enum BcPoint {
     /// Concentrated load parallel to z
     Fz,
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::{BcPoint, Nbc};
+
+    #[test]
+    fn derive_works() {
+        let qn = Nbc::Qn;
+        let qn_cloned = qn.clone();
+        let qn_copy = qn;
+        assert_eq!(format!("{:?}", qn), "Qn");
+        assert_eq!(qn, qn_cloned);
+        assert_eq!(qn, qn_copy);
+
+        let fx = BcPoint::Fx;
+        let fx_cloned = fx.clone();
+        let fx_copy = fx;
+        assert_eq!(format!("{:?}", fx), "Fx");
+        assert_eq!(fx, fx_cloned);
+        assert_eq!(fx, fx_copy);
+    }
+}
