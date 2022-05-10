567fc5bfe8cddc867ce0f72b0925aa8c67bd02fb
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-05-10 17:54:28 +1000

    Improve doc of bcs

--- src/base/boundary_conditions.rs
+++ src/base/boundary_conditions.rs
@@ -1,16 +1,28 @@
 /// Defines natural boundary conditions (NBC)
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub enum Nbc {
+    /// Normal distributed load
     Qn,
+
+    /// Distributed load parallel to x
     Qx,
+
+    /// Distributed load parallel to y
     Qy,
+
+    /// Distributed load parallel to z
     Qz,
 }
 
 /// Defines boundary conditions at points (e.g., point loads)
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub enum BcPoint {
+    /// Concentrated load parallel to x
     Fx,
+
+    /// Concentrated load parallel to y
     Fy,
+
+    /// Concentrated load parallel to z
     Fz,
 }
