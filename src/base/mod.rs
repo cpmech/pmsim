745ee918c7ad26297208692a9073de1e93c31451
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-07-04 18:34:03 +1000

    Join enums in a single file

--- src/base/mod.rs
+++ src/base/mod.rs
@@ -1,10 +1,6 @@
 //! Implements the base structures for a finite element simulation
 
-mod boundary_conditions;
-mod degrees_of_freedom;
-mod initialization_option;
+mod enums;
 mod parameters;
-pub use crate::base::boundary_conditions::*;
-pub use crate::base::degrees_of_freedom::*;
-pub use crate::base::initialization_option::*;
+pub use crate::base::enums::*;
 pub use crate::base::parameters::*;
