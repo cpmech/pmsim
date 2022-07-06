66d329c5e9e243e9d239b855c00981024f7eba25
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-07-07 00:30:43 +1000

    Improve Assemble

--- src/base/assemble.rs
+++ src/base/assemble.rs
@@ -1,26 +1,35 @@
-#![allow(unused)]
-
-use super::Equation;
-use gemlab::mesh::CellId;
+use super::{Dof, Equation};
+use gemlab::mesh::{CellId, Mesh};
 use russell_lab::Vector;
-use std::collections::HashSet;
 
 pub struct Assemble {
     /// Holds all local-to-global mappings (ncell x n_local_equation)
     pub local_to_global: Vec<Vec<usize>>,
+
+    /// Records what equations are prescribed (nequation)
+    pub prescribed: Vec<bool>,
 }
 
 impl Assemble {
-    pub fn new(ncell: usize) -> Self {
+    // cell_dofs [ncell][nnode]
+    pub fn new(mesh: &Mesh, equation: &Equation, cell_dofs: &Vec<Vec<Dof>>) -> Self {
+        let mut local_to_global = vec![Vec::new(); mesh.cells.len()];
+        for cell in &mesh.cells {
+            for m in 0..cell.points.len() {
+                let eid = equation.id(cell.points[m], cell_dofs[cell.id][m]).unwrap();
+                local_to_global[cell.id].push(eid);
+            }
+        }
         Assemble {
-            local_to_global: vec![Vec::new(); ncell],
+            local_to_global,
+            prescribed: vec![false; equation.len()],
         }
     }
 
-    pub fn vector(&self, global: &mut Vector, local: &Vector, cell_id: CellId, prescribed: &Vec<bool>) {
+    pub fn vector(&self, global: &mut Vector, local: &Vector, cell_id: CellId) {
         for i in 0..local.dim() {
             let g = self.local_to_global[cell_id][i];
-            if !prescribed[g] {
+            if !self.prescribed[g] {
                 global[g] += local[i];
             }
         }
@@ -31,45 +40,43 @@ impl Assemble {
 
 #[cfg(test)]
 mod tests {
+    use super::Assemble;
+    use crate::base::{Dof, Equation, SampleMeshes};
     use russell_lab::Vector;
 
-    use super::Assemble;
-    use crate::base::SampleMeshes;
-
     #[test]
-    fn new_works() {
-        let assemble = Assemble::new(2);
-        assert_eq!(assemble.local_to_global.len(), 2);
-    }
-
-    #[test]
-    fn vector_works() {
-        //         4---.__
-        //        / \     `--.___3
-        //       /   \          / \
-        //      /     \  [1]   /   \
-        //     /  [0]  \      /     \
-        //    /         \    /  [2]  \
-        //   0---.__     \  /      ___2
-        //          `--.__\/__.---'
-        //                 1
+    fn new_and_vector_works() {
+        //       {2}4---.__
+        //         / \     `--.___3{3}
+        //        /   \          / \
+        //       /     \  [1]   /   \    {#} means equation id
+        //      /  [0]  \      /     \
+        //     /         \    /  [2]  \
+        // {0}0---.__     \  /      ___2{4}
+        //           `--.__\/__.---'
+        //               {1}1
         let mesh = SampleMeshes::three_tri3();
-        let mut assemble = Assemble::new(mesh.cells.len());
-        for cell in &mesh.cells {
-            for point_id in &cell.points {
-                assemble.local_to_global[cell.id].push(*point_id);
-            }
-        }
-        assert_eq!(assemble.local_to_global, &[[0, 1, 4], [1, 3, 4], [1, 2, 3]]);
+        let mut equation = Equation::new(mesh.points.len());
+        let cell_dofs = vec![
+            vec![Dof::T, Dof::T, Dof::T],
+            vec![Dof::T, Dof::T, Dof::T],
+            vec![Dof::T, Dof::T, Dof::T],
+        ];
+        mesh.cells.iter().zip(&cell_dofs).for_each(|(cell, dofs)| {
+            cell.points.iter().zip(dofs).for_each(|(point_id, dof)| {
+                equation.activate(*point_id, *dof);
+            });
+        });
+        let assemble = Assemble::new(&mesh, &equation, &cell_dofs);
+        assert_eq!(assemble.local_to_global, &[[0, 1, 2], [1, 3, 2], [1, 4, 3]]);
 
-        let mut global = Vector::new(5);
-        let prescribed = vec![false; global.dim()];
+        let mut global = Vector::new(equation.len());
         let local_0 = Vector::from(&[/*    */ 10.0, /*    */ 11.0, /*    */ 14.0]);
         let local_1 = Vector::from(&[/*  */ 2100.0, /*  */ 2300.0, /*  */ 2400.0]);
         let local_2 = Vector::from(&[/**/ 310000.0, /**/ 320000.0, /**/ 330000.0]);
-        assemble.vector(&mut global, &local_0, 0, &prescribed);
-        assemble.vector(&mut global, &local_1, 1, &prescribed);
-        assemble.vector(&mut global, &local_2, 2, &prescribed);
-        assert_eq!(global.as_data(), &[10.0, 312111.0, 320000.0, 332300.0, 2414.0]);
+        assemble.vector(&mut global, &local_0, 0);
+        assemble.vector(&mut global, &local_1, 1);
+        assemble.vector(&mut global, &local_2, 2);
+        assert_eq!(global.as_data(), &[10.0, 312111.0, 2414.0, 332300.0, 320000.0]);
     }
 }
