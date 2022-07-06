428e4c6b90ef7859752dc0e259bec4b3e4da5081
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-07-06 16:48:41 +1000

    Remove Region from Config

--- src/base/config.rs
+++ src/base/config.rs
@@ -1,17 +1,11 @@
 use super::{Init, ParamElement, ParamFluids};
 use crate::StrError;
-use gemlab::mesh::{CellAttributeId, Region};
+use gemlab::mesh::CellAttributeId;
 use std::collections::HashMap;
 use std::fmt;
 
 /// Holds configuration data such as boundary conditions and element attributes
-pub struct Config<'a> {
-    /// The finite element region/mesh
-    pub region: &'a Region<'a>,
-
-    /// Space number of dimensions
-    pub ndim: usize,
-
+pub struct Config {
     /// Parameters for fluids
     pub param_fluids: Option<ParamFluids>,
 
@@ -34,12 +28,10 @@ pub struct Config<'a> {
     pub initialization: Init,
 }
 
-impl<'a> Config<'a> {
+impl Config {
     /// Allocates a new instance
-    pub fn new(region: &'a Region<'a>) -> Self {
+    pub fn new() -> Self {
         Config {
-            region,
-            ndim: region.mesh.ndim,
             param_fluids: None,
             param_elements: HashMap::new(),
             gravity: 0.0,
@@ -88,9 +80,6 @@ impl<'a> Config<'a> {
     ///
     /// **Note:** If false (plane-strain), this function will set the thickness to 1.0.
     pub fn plane_stress(&mut self, flag: bool) -> Result<&mut Self, StrError> {
-        if self.ndim == 3 {
-            return Err("cannot set plane_stress in 3D");
-        }
         match self.initialization {
             Init::Geostatic(..) => return Err("cannot set plane_stress with Init::Geostatic"),
             Init::Isotropic(..) => return Err("cannot set plane_stress with Init::Isotropic"),
@@ -141,20 +130,10 @@ impl<'a> Config<'a> {
     }
 }
 
-impl<'a> fmt::Display for Config<'a> {
+impl fmt::Display for Config {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Mesh data\n").unwrap();
-        write!(f, "=========\n").unwrap();
-        write!(f, "ndim = {}\n", self.region.mesh.ndim).unwrap();
-        write!(f, "npoint = {}\n", self.region.mesh.points.len()).unwrap();
-        write!(f, "ncell = {}\n", self.region.mesh.cells.len()).unwrap();
-        write!(f, "nedge = {}\n", self.region.features.edges.len()).unwrap();
-        if self.ndim == 3 {
-            write!(f, "nface = {}\n", self.region.features.faces.len()).unwrap();
-        }
-
-        write!(f, "\nOther configuration data\n").unwrap();
-        write!(f, "========================\n").unwrap();
+        write!(f, "Configuration data\n").unwrap();
+        write!(f, "==================\n").unwrap();
         write!(f, "gravity = {:?}\n", self.gravity).unwrap();
         write!(f, "thickness = {:?}\n", self.thickness).unwrap();
         write!(f, "plane_stress = {:?}\n", self.plane_stress).unwrap();
@@ -182,16 +161,16 @@ impl<'a> fmt::Display for Config<'a> {
 #[cfg(test)]
 mod tests {
     use super::Config;
-    use crate::base::{Init, ParamElement, ParamFluids, ParamRealDensity, ParamSolid, ParamStressStrain, SampleMeshes};
+    use crate::base::{Init, ParamElement, ParamFluids, ParamRealDensity, ParamSolid, ParamStressStrain};
     use crate::StrError;
-    use gemlab::mesh::{Extract, Region};
 
     #[test]
-    fn new_works_2d() -> Result<(), StrError> {
-        let mesh = SampleMeshes::two_tri3();
-        let region = Region::with(&mesh, Extract::Boundary)?;
+    fn new_works() -> Result<(), StrError> {
+        let mut config = Config::new();
+        config.init(Init::Zero)?;
+        assert_eq!(config.initial_overburden_stress(), 0.0);
 
-        let mut config = Config::new(&region);
+        let mut config = Config::new();
 
         let fluids = ParamFluids {
             density_liquid: ParamRealDensity {
@@ -226,15 +205,8 @@ mod tests {
 
         assert_eq!(
             format!("{}", config),
-            "Mesh data\n\
-             =========\n\
-             ndim = 2\n\
-             npoint = 4\n\
-             ncell = 2\n\
-             nedge = 4\n\
-             \n\
-             Other configuration data\n\
-             ========================\n\
+            "Configuration data\n\
+             ==================\n\
              gravity = 10.0\n\
              thickness = 1.0\n\
              plane_stress = false\n\
@@ -249,66 +221,13 @@ mod tests {
              =======================\n\
              1 → Solid(ParamSolid { density: 2.7, stress_strain: LinearElastic { young: 10000.0, poisson: 0.2 }, n_integ_point: None })\n"
         );
-        Ok(())
-    }
 
-    #[test]
-    fn new_works_3d() -> Result<(), StrError> {
-        let mesh = SampleMeshes::one_cube();
-        let region = Region::with(&mesh, Extract::Boundary)?;
-
-        let mut config = Config::new(&region);
-
-        let solid = ParamSolid {
-            density: 2.7, // Mg/m²
-            stress_strain: ParamStressStrain::LinearElastic {
-                young: 10_000.0, // kPa
-                poisson: 0.2,    // [-]
-            },
-            n_integ_point: None,
-        };
-
-        config
-            .elements(1, ParamElement::Solid(solid))?
-            .gravity(10.0)? // m/s²
-            .init(Init::Zero)?;
-
-        assert_eq!(config.initial_overburden_stress(), 0.0);
-
-        assert_eq!(
-            format!("{}", config),
-            "Mesh data\n\
-             =========\n\
-             ndim = 3\n\
-             npoint = 8\n\
-             ncell = 1\n\
-             nedge = 12\n\
-             nface = 6\n\
-             \n\
-             Other configuration data\n\
-             ========================\n\
-             gravity = 10.0\n\
-             thickness = 1.0\n\
-             plane_stress = false\n\
-             total_stress = false\n\
-             initialization = Zero\n\
-             \n\
-             Parameters for fluids\n\
-             =====================\n\
-             None\n\
-             \n\
-             Parameters for Elements\n\
-             =======================\n\
-             1 → Solid(ParamSolid { density: 2.7, stress_strain: LinearElastic { young: 10000.0, poisson: 0.2 }, n_integ_point: None })\n"
-        );
         Ok(())
     }
 
     #[test]
     fn catch_some_errors_2d() -> Result<(), StrError> {
-        let mesh = SampleMeshes::two_tri3();
-        let region = Region::with(&mesh, Extract::Boundary)?;
-        let mut config = Config::new(&region);
+        let mut config = Config::new();
         assert_eq!(config.gravity(-10.0).err(), Some("gravity must be ≥ 0.0"));
         assert_eq!(config.thickness(0.0).err(), Some("thickness must be > 0.0"));
         assert_eq!(
@@ -335,15 +254,6 @@ mod tests {
             config.plane_stress(true).err(),
             Some("cannot set plane_stress with Init::Isotropic")
         );
-        Ok(())
-    }
-
-    #[test]
-    fn catch_some_errors_3d() -> Result<(), StrError> {
-        let mesh = SampleMeshes::one_cube();
-        let region = Region::with(&mesh, Extract::Boundary)?;
-        let mut config = Config::new(&region);
-        assert_eq!(config.plane_stress(true).err(), Some("cannot set plane_stress in 3D"));
         Ok(())
     }
 }
