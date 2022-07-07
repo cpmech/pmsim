5ffb7c65db33ddddef8ff7074e6a59ecbf052c25
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-07-07 22:09:11 +1000

    Improve NodalDofs

--- src/base/nodal_dofs.rs
+++ src/base/nodal_dofs.rs
@@ -1,38 +1,63 @@
-#![allow(unused)]
-
 use super::{Dof, Element, POROUS_SLD_GEO_KIND_ALLOWED};
-use gemlab::shapes::{GeoClass, GeoKind};
+use crate::StrError;
+use gemlab::shapes::GeoKind;
 use std::collections::HashMap;
 use std::fmt;
 
+/// Holds the nodal degrees-of-freedom for a pair of (Element,GeoKind)
+///
+/// Example of DOFs for ([super::Element::PorousSldLiq],[gemlab::shapes::Tri6]):
+///
+/// ```text
+///              {Ux,Uy,Pl}
+///                  2
+///                 / \
+///                /   \
+///       {Ux,Uy} 5     4 {Ux,Uy}
+///              /       \
+///             /         \
+/// {Ux,Uy,Pl} 0-----3-----1 {Ux,Uy,Pl}
+///               {Ux,Uy}
+/// ```
 pub struct NodalDofs {
-    pub all: HashMap<(Element, GeoKind), Vec<Vec<Dof>>>,
+    /// Space number of dimension (for verification purposes)
+    ndim: usize,
+
+    /// Holds all combinations
+    all: HashMap<(Element, GeoKind), Vec<Vec<Dof>>>,
 }
 
 impl NodalDofs {
-    pub fn new() -> Self {
-        NodalDofs { all: HashMap::new() }
+    /// Allocates a new instance
+    pub fn new(ndim: usize) -> Self {
+        NodalDofs {
+            ndim,
+            all: HashMap::new(),
+        }
     }
 
-    pub fn set(&mut self, ndim: usize, element: Element, kind: GeoKind) {
+    pub fn set(&mut self, element: Element, kind: GeoKind) -> Result<(), StrError> {
         // check consistency
         let rod_or_beam = element == Element::Rod || element == Element::Beam;
         let lin_geometry = kind.is_lin();
-        if (rod_or_beam && !lin_geometry) || (!rod_or_beam && lin_geometry) {
-            return; // inconsistent combination
+        if rod_or_beam && !lin_geometry {
+            return Err("cannot set Rod or Beam with a non-Lin GeoClass"); // inconsistent combination
+        }
+        if !rod_or_beam && lin_geometry {
+            return Err("GeoClass::Lin is reserved for Rod or Beam"); // inconsistent combination
         }
 
         // check for existent entry
         let key = &(element, kind);
         if self.all.contains_key(key) {
-            return; // already configured
+            return Ok(()); // already configured
         }
 
         // update HashMap
         let nnode = kind.nnode();
         let dofs = match element {
             Element::Rod => {
-                let dofs_per_node = if ndim == 2 {
+                let dofs_per_node = if self.ndim == 2 {
                     vec![Dof::Ux, Dof::Uy]
                 } else {
                     vec![Dof::Ux, Dof::Uy, Dof::Uz]
@@ -40,7 +65,7 @@ impl NodalDofs {
                 vec![dofs_per_node; nnode]
             }
             Element::Beam => {
-                let dofs_per_node = if ndim == 2 {
+                let dofs_per_node = if self.ndim == 2 {
                     vec![Dof::Ux, Dof::Uy, Dof::Rz]
                 } else {
                     vec![Dof::Ux, Dof::Uy, Dof::Uz, Dof::Rx, Dof::Ry, Dof::Rz]
@@ -48,7 +73,7 @@ impl NodalDofs {
                 vec![dofs_per_node; nnode]
             }
             Element::Solid => {
-                let dofs_per_node = if ndim == 2 {
+                let dofs_per_node = if self.ndim == 2 {
                     vec![Dof::Ux, Dof::Uy]
                 } else {
                     vec![Dof::Ux, Dof::Uy, Dof::Uz]
@@ -64,11 +89,10 @@ impl NodalDofs {
                 vec![dofs_per_node; nnode]
             }
             Element::PorousSldLiq => {
-                let index = match POROUS_SLD_GEO_KIND_ALLOWED.iter().position(|k| *k == kind) {
-                    Some(i) => i,
-                    None => return, // not allowed
+                if !POROUS_SLD_GEO_KIND_ALLOWED.contains(&kind) {
+                    return Err("cannot set PorousSldLiq with given GeoKind");
                 };
-                let high_order_dofs_per_node = if ndim == 2 {
+                let high_order_dofs_per_node = if self.ndim == 2 {
                     vec![Dof::Ux, Dof::Uy]
                 } else {
                     vec![Dof::Ux, Dof::Uy, Dof::Uz]
@@ -81,11 +105,10 @@ impl NodalDofs {
                 dofs
             }
             Element::PorousSldLiqGas => {
-                let index = match POROUS_SLD_GEO_KIND_ALLOWED.iter().position(|k| *k == kind) {
-                    Some(i) => i,
-                    None => return, // not allowed
+                if !POROUS_SLD_GEO_KIND_ALLOWED.contains(&kind) {
+                    return Err("cannot set PorousSldLiqGas with given GeoKind");
                 };
-                let high_order_dofs_per_node = if ndim == 2 {
+                let high_order_dofs_per_node = if self.ndim == 2 {
                     vec![Dof::Ux, Dof::Uy]
                 } else {
                     vec![Dof::Ux, Dof::Uy, Dof::Uz]
@@ -100,13 +123,7 @@ impl NodalDofs {
             }
         };
         self.all.insert(*key, dofs);
-    }
-
-    pub fn validate(&self) -> Option<String> {
-        for ((element, kind), dofs) in &self.all {
-            //
-        }
-        None // all good
+        Ok(())
     }
 }
 
@@ -131,18 +148,45 @@ impl fmt::Display for NodalDofs {
 mod tests {
     use super::NodalDofs;
     use crate::base::Element;
+    use crate::StrError;
     use gemlab::shapes::GeoKind;
 
     #[test]
-    pub fn nodal_dofs_work() {
-        let mut nodal_dofs = NodalDofs::new();
-        nodal_dofs.set(2, Element::Rod, GeoKind::Lin2);
-        nodal_dofs.set(2, Element::Beam, GeoKind::Lin2);
-        nodal_dofs.set(2, Element::Solid, GeoKind::Tri3);
-        nodal_dofs.set(2, Element::PorousLiq, GeoKind::Tri3);
-        nodal_dofs.set(2, Element::PorousLiqGas, GeoKind::Tri3);
-        nodal_dofs.set(2, Element::PorousSldLiq, GeoKind::Tri6);
-        nodal_dofs.set(2, Element::PorousSldLiqGas, GeoKind::Tri6);
+    pub fn set_captures_errors() {
+        let mut nodal_dofs = NodalDofs::new(2);
+        assert_eq!(
+            nodal_dofs.set(Element::Rod, GeoKind::Tri3).err(),
+            Some("cannot set Rod or Beam with a non-Lin GeoClass")
+        );
+        assert_eq!(
+            nodal_dofs.set(Element::Beam, GeoKind::Tri3).err(),
+            Some("cannot set Rod or Beam with a non-Lin GeoClass")
+        );
+        assert_eq!(
+            nodal_dofs.set(Element::Solid, GeoKind::Lin2).err(),
+            Some("GeoClass::Lin is reserved for Rod or Beam")
+        );
+        assert_eq!(
+            nodal_dofs.set(Element::PorousSldLiq, GeoKind::Tri3).err(),
+            Some("cannot set PorousSldLiq with given GeoKind")
+        );
+        assert_eq!(
+            nodal_dofs.set(Element::PorousSldLiqGas, GeoKind::Tri3).err(),
+            Some("cannot set PorousSldLiqGas with given GeoKind")
+        );
+    }
+
+    #[test]
+    pub fn set_works_2d() -> Result<(), StrError> {
+        let mut nodal_dofs = NodalDofs::new(2);
+        nodal_dofs.set(Element::Rod, GeoKind::Lin2)?;
+        nodal_dofs.set(Element::Beam, GeoKind::Lin2)?;
+        nodal_dofs.set(Element::Solid, GeoKind::Tri3)?;
+        nodal_dofs.set(Element::PorousLiq, GeoKind::Tri3)?;
+        nodal_dofs.set(Element::PorousLiq, GeoKind::Tri3)?; // already configured
+        nodal_dofs.set(Element::PorousLiqGas, GeoKind::Tri3)?;
+        nodal_dofs.set(Element::PorousSldLiq, GeoKind::Tri6)?;
+        nodal_dofs.set(Element::PorousSldLiqGas, GeoKind::Tri6)?;
         assert_eq!(format!("{}", nodal_dofs),
             "Nodal Dofs\n\
              ==========\n\
@@ -154,5 +198,31 @@ mod tests {
              (PorousSldLiq, Tri6) → [[Ux, Uy, Pl], [Ux, Uy, Pl], [Ux, Uy, Pl], [Ux, Uy], [Ux, Uy], [Ux, Uy]]\n\
              (PorousSldLiqGas, Tri6) → [[Ux, Uy, Pl, Pg], [Ux, Uy, Pl, Pg], [Ux, Uy, Pl, Pg], [Ux, Uy], [Ux, Uy], [Ux, Uy]]\n"
         );
+        Ok(())
+    }
+
+    #[test]
+    pub fn set_works_3d() -> Result<(), StrError> {
+        let mut nodal_dofs = NodalDofs::new(3);
+        nodal_dofs.set(Element::Rod, GeoKind::Lin2)?;
+        nodal_dofs.set(Element::Beam, GeoKind::Lin2)?;
+        nodal_dofs.set(Element::Solid, GeoKind::Tri3)?;
+        nodal_dofs.set(Element::PorousLiq, GeoKind::Tri3)?;
+        nodal_dofs.set(Element::PorousLiq, GeoKind::Tri3)?; // already configured
+        nodal_dofs.set(Element::PorousLiqGas, GeoKind::Tri3)?;
+        nodal_dofs.set(Element::PorousSldLiq, GeoKind::Tri6)?;
+        nodal_dofs.set(Element::PorousSldLiqGas, GeoKind::Tri6)?;
+        assert_eq!(format!("{}", nodal_dofs),
+            "Nodal Dofs\n\
+             ==========\n\
+             (Rod, Lin2) → [[Ux, Uy, Uz], [Ux, Uy, Uz]]\n\
+             (Beam, Lin2) → [[Ux, Uy, Uz, Rx, Ry, Rz], [Ux, Uy, Uz, Rx, Ry, Rz]]\n\
+             (Solid, Tri3) → [[Ux, Uy, Uz], [Ux, Uy, Uz], [Ux, Uy, Uz]]\n\
+             (PorousLiq, Tri3) → [[Pl], [Pl], [Pl]]\n\
+             (PorousLiqGas, Tri3) → [[Pl, Pg], [Pl, Pg], [Pl, Pg]]\n\
+             (PorousSldLiq, Tri6) → [[Ux, Uy, Uz, Pl], [Ux, Uy, Uz, Pl], [Ux, Uy, Uz, Pl], [Ux, Uy, Uz], [Ux, Uy, Uz], [Ux, Uy, Uz]]\n\
+             (PorousSldLiqGas, Tri6) → [[Ux, Uy, Uz, Pl, Pg], [Ux, Uy, Uz, Pl, Pg], [Ux, Uy, Uz, Pl, Pg], [Ux, Uy, Uz], [Ux, Uy, Uz], [Ux, Uy, Uz]]\n"
+        );
+        Ok(())
     }
 }
