d52c242f9e17f068ab442ed486305c8a89732390
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-07-08 23:02:26 +1000

    Impl Bhatti's example 1.4

--- src/base/sample_meshes.rs
+++ src/base/sample_meshes.rs
@@ -149,6 +149,44 @@ impl SampleMeshes {
             ],
         }
     }
+
+    /// Returns the mesh used in Example 1.4 of Bhatti's book
+    ///
+    /// ```text
+    ///               (3)
+    ///               [2]
+    ///     2----------------------3
+    ///     |'.  (4)           _.-'
+    ///     |  '.[3]       _.-'
+    ///     |    '.    _.-'  (1)
+    /// (2) |      '1-'      [1]
+    /// [2] |      /
+    ///     |     /
+    ///     |    / (0)   The lines are ROD (Lin2) elements
+    ///     |   /  [1]
+    ///     |  /
+    ///     | /    (#) indicates cell id
+    ///     0'     [#] indicates attribute id
+    /// ```
+    #[rustfmt::skip]
+    pub fn bhatti_example_1dot4_truss() -> Mesh {
+        Mesh {
+            ndim: 2,
+            points: vec![
+                Point { id: 0, coords: vec![   0.0,    0.0] },
+                Point { id: 1, coords: vec![1500.0, 3500.0] },
+                Point { id: 2, coords: vec![   0.0, 5000.0] },
+                Point { id: 3, coords: vec![5000.0, 5000.0] },
+            ],
+            cells: vec![
+                Cell { id: 0, attribute_id: 1, kind: GeoKind::Lin2, points: vec![0, 1] },
+                Cell { id: 1, attribute_id: 1, kind: GeoKind::Lin2, points: vec![1, 3] },
+                Cell { id: 2, attribute_id: 2, kind: GeoKind::Lin2, points: vec![0, 2] },
+                Cell { id: 3, attribute_id: 2, kind: GeoKind::Lin2, points: vec![2, 3] },
+                Cell { id: 4, attribute_id: 3, kind: GeoKind::Lin2, points: vec![2, 1] },
+            ],
+        }
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -177,5 +215,9 @@ mod tests {
         let mesh = SampleMeshes::one_hex8();
         assert_eq!(mesh.cells.len(), 1);
         // draw_mesh(&mesh, true, "/tmp/pmsim/test_mesh_one_hex8.svg").unwrap();
+
+        let mesh = SampleMeshes::bhatti_example_1dot4_truss();
+        assert_eq!(mesh.cells.len(), 5);
+        // TODO: fix gemlab. draw_mesh(&mesh, true, "/tmp/pmsim/test_mesh_bhatti_example_1dot4_truss.svg").unwrap();
     }
 }
