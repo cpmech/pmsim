974495ea1b00c61d39ff3f08faf649ed850389be
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-07-20 23:30:24 +1000

    [wip] Impl DofNumbers

---
+++ src/base/dof_numbers.rs
@@ -0,0 +1,223 @@
+use super::{Dof, Element, POROUS_SLD_GEO_KIND_ALLOWED};
+use crate::StrError;
+use gemlab::mesh::{CellAttributeId, Mesh};
+use gemlab::shapes::GeoKind;
+use std::collections::{HashMap, HashSet};
+use std::fmt;
+
+/// Defines the equation ID
+type Eid = usize;
+
+/// Defines the total number of equations
+type Total = usize;
+
+/// Defines a nested array with DOFs and equation IDs
+type DofEid = Vec<Vec<(Dof, Eid)>>;
+
+/// Defines a type holding the DOFs, equation IDs, and the total number of equations
+type DofEidTotal = (DofEid, Total);
+
+/// Returns the DOFs and local equation numbers of a (Element,GeoKind) pair
+#[rustfmt::skip]
+fn get_cell_dofs(ndim: usize, element: Element, kind: GeoKind) -> Result<DofEidTotal, StrError> {
+    let rod_or_beam = element == Element::Rod || element == Element::Beam;
+    let lin_geometry = kind.is_lin();
+    if rod_or_beam && !lin_geometry {
+        return Err("cannot set Rod or Beam with a non-Lin GeoClass"); // inconsistent combination
+    }
+    if !rod_or_beam && lin_geometry {
+        return Err("GeoClass::Lin is reserved for Rod or Beam"); // inconsistent combination
+    }
+    let nnode = kind.nnode();
+    let mut total = 0; // local equation ID = total number of local equations in the end
+    let mut cell_dofs: Vec<Vec<(Dof, Eid)>> = vec![Vec::new(); nnode];
+    match element {
+        Element::Rod => {
+            for m in 0..nnode {
+                cell_dofs[m].push((Dof::Ux, total)); total += 1;
+                cell_dofs[m].push((Dof::Uy, total)); total += 1;
+                if ndim == 3 {
+                    cell_dofs[m].push((Dof::Uz, total)); total += 1;
+                }
+            }
+        }
+        Element::Beam => {
+            for m in 0..nnode {
+                cell_dofs[m].push((Dof::Ux, total)); total += 1;
+                cell_dofs[m].push((Dof::Uy, total)); total += 1;
+                if ndim == 2 {
+                    cell_dofs[m].push((Dof::Rz, total)); total += 1;
+                } else {
+                    cell_dofs[m].push((Dof::Uz, total)); total += 1;
+                    cell_dofs[m].push((Dof::Rx, total)); total += 1;
+                    cell_dofs[m].push((Dof::Ry, total)); total += 1;
+                    cell_dofs[m].push((Dof::Rz, total)); total += 1;
+                }
+            }
+        }
+        Element::Solid => {
+            for m in 0..nnode {
+                cell_dofs[m].push((Dof::Ux, total)); total += 1;
+                cell_dofs[m].push((Dof::Uy, total)); total += 1;
+                if ndim == 3 {
+                    cell_dofs[m].push((Dof::Uz, total)); total += 1;
+                }
+            }
+        }
+        Element::PorousLiq => {
+            for m in 0..nnode {
+                cell_dofs[m].push((Dof::Pl, total)); total += 1;
+            }
+        }
+        Element::PorousLiqGas => {
+            for m in 0..nnode {
+                cell_dofs[m].push((Dof::Pl, total)); total += 1;
+                cell_dofs[m].push((Dof::Pg, total)); total += 1;
+            }
+        }
+        Element::PorousSldLiq => {
+            if !POROUS_SLD_GEO_KIND_ALLOWED.contains(&kind) {
+                return Err("cannot set PorousSldLiq with given GeoKind");
+            };
+            for m in 0..nnode {
+                cell_dofs[m].push((Dof::Ux, total)); total += 1;
+                cell_dofs[m].push((Dof::Uy, total)); total += 1;
+                if ndim == 3 {
+                    cell_dofs[m].push((Dof::Uz, total)); total += 1;
+                }
+            }
+            let ncorner = kind.lower_order().unwrap().nnode();
+            for m in 0..ncorner {
+                cell_dofs[m].push((Dof::Pl, total)); total += 1;
+            }
+        }
+        Element::PorousSldLiqGas => {
+            if !POROUS_SLD_GEO_KIND_ALLOWED.contains(&kind) {
+                return Err("cannot set PorousSldLiqGas with given GeoKind");
+            };
+            for m in 0..nnode {
+                cell_dofs[m].push((Dof::Ux, total)); total += 1;
+                cell_dofs[m].push((Dof::Uy, total)); total += 1;
+                if ndim == 3 {
+                    cell_dofs[m].push((Dof::Uz, total)); total += 1;
+                }
+            }
+            let ncorner = kind.lower_order().unwrap().nnode();
+            for m in 0..ncorner {
+                cell_dofs[m].push((Dof::Pl, total)); total += 1;
+                cell_dofs[m].push((Dof::Pg, total)); total += 1;
+            }
+        }
+    };
+    Ok((cell_dofs, total))
+}
+
+pub struct DofNumbers {
+    point_dof_eid: Vec<HashMap<Dof, Eid>>,
+    local_to_global: Vec<Vec<Eid>>,
+    neq: usize,
+}
+
+impl DofNumbers {
+    pub fn new(mesh: &Mesh, att_ele: &HashMap<CellAttributeId, Element>) -> Result<Self, StrError> {
+        // auxiliary memoization data
+        let npoint = mesh.points.len();
+        let mut memo_cell_dofs: HashMap<(CellAttributeId, GeoKind), DofEidTotal> = HashMap::new();
+        let mut memo_point_dofs: Vec<HashSet<Dof>> = vec![HashSet::new(); npoint];
+
+        // find the DOFs and local equation numbers of all cells
+        for cell in &mesh.cells {
+            let element = match att_ele.get(&cell.attribute_id) {
+                Some(e) => e,
+                None => return Err("cannot find attribute in att_ele map"),
+            };
+            let (cell_dofs, _) = memo_cell_dofs
+                .entry((cell.attribute_id, cell.kind))
+                .or_insert(get_cell_dofs(mesh.ndim, *element, cell.kind)?);
+            for m in 0..cell.points.len() {
+                for (dof, _) in &cell_dofs[m] {
+                    memo_point_dofs[cell.points[m]].insert(*dof);
+                }
+            }
+        }
+
+        // compute all points' DOFs and global equation ids
+        let mut point_dof_eid: Vec<HashMap<Dof, Eid>> = vec![HashMap::new(); npoint];
+        let mut total = 0; // total number of global equations
+        for point_id in 0..npoint {
+            let mut sorted_dofs: Vec<_> = memo_point_dofs[point_id].iter().collect();
+            sorted_dofs.sort();
+            for dof in sorted_dofs {
+                point_dof_eid[point_id].insert(*dof, total);
+                total += 1;
+            }
+        }
+
+        // compute all cells' local_to_global maps
+        let ncell = mesh.cells.len();
+        let mut local_to_global: Vec<Vec<Eid>> = vec![Vec::new(); ncell];
+        for cell in &mesh.cells {
+            let (cell_dofs, n_local_eqn) = memo_cell_dofs.get(&(cell.attribute_id, cell.kind)).unwrap();
+            local_to_global[cell.id] = vec![0; *n_local_eqn];
+            for m in 0..cell.points.len() {
+                for (dof, local_eid) in &cell_dofs[m] {
+                    let global_eid = *point_dof_eid[cell.points[m]].get(dof).unwrap();
+                    local_to_global[cell.id][*local_eid] = global_eid;
+                }
+            }
+        }
+
+        // done
+        Ok(DofNumbers {
+            point_dof_eid,
+            local_to_global,
+            neq: total,
+        })
+    }
+}
+
+impl fmt::Display for DofNumbers {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let thick_line = "================================================================================\n";
+        let thin_line = "--------------------------------------------------------------------------------\n";
+
+        f.write_str(thick_line).unwrap();
+        write!(f, "number of equations = {}\n", self.neq).unwrap();
+
+        f.write_str(thin_line).unwrap();
+        write!(f, "PointId: (Dof, EquationId)\n").unwrap();
+        for point_id in 0..self.point_dof_eid.len() {
+            let mut dof_eqn: Vec<_> = self.point_dof_eid[point_id].iter().collect();
+            dof_eqn.sort_by(|a, b| a.0.partial_cmp(b.0).unwrap());
+            write!(f, "{:?}: {:?}\n", point_id, dof_eqn).unwrap();
+        }
+
+        f.write_str(thin_line).unwrap();
+        write!(f, "CellId: Local-to-Global\n").unwrap();
+        for cell_id in 0..self.local_to_global.len() {
+            write!(f, "{:?}: {:?}\n", cell_id, self.local_to_global[cell_id]).unwrap();
+        }
+
+        f.write_str(thick_line)
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::DofNumbers;
+    use crate::base::Element;
+    use gemlab::mesh::Samples;
+    use gemlab::StrError;
+    use std::collections::HashMap;
+
+    #[test]
+    fn new_works() -> Result<(), StrError> {
+        let mesh = Samples::qua8_tri6_lin2();
+        let att_ele = HashMap::from([(1, Element::PorousSldLiq), (2, Element::Solid), (3, Element::Beam)]);
+        let dof_numbers = DofNumbers::new(&mesh, &att_ele)?;
+        println!("{}", dof_numbers);
+        Ok(())
+    }
+}
