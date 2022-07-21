d91196a7531d29414cf7a782b1c46041ad94d855
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-07-21 10:48:59 +1000

    Organize DofNumbers. Rename vars

--- src/base/dof_numbers.rs
+++ src/base/dof_numbers.rs
@@ -5,21 +5,15 @@ use gemlab::shapes::GeoKind;
 use std::collections::{HashMap, HashSet};
 use std::fmt;
 
-/// Defines the equation ID
-type Eid = usize;
+/// Defines a nested array with DOF keys and numbers
+///
+/// * The outer array has length = npoint (or nnode)
+/// * The inner arrays have variable lengths = ndof at the point (or node)
+pub type ArrayDofNum = Vec<Vec<(Dof, usize)>>;
 
-/// Defines the total number of equations
-type Total = usize;
-
-/// Defines a nested array with DOFs and equation IDs
-type DofEid = Vec<Vec<(Dof, Eid)>>;
-
-/// Defines a type holding the DOFs, equation IDs, and the total number of equations
-type DofEidTotal = (DofEid, Total);
-
-/// Returns the DOFs and local equation numbers of a (Element,GeoKind) pair
+/// Returns the DOF keys and numbers for each cell node and the total number of local dofs
 #[rustfmt::skip]
-fn get_cell_dofs(ndim: usize, element: Element, kind: GeoKind) -> Result<DofEidTotal, StrError> {
+fn get_cell_dofs(ndim: usize, element: Element, kind: GeoKind) -> Result<(ArrayDofNum, usize), StrError> {
     let rod_or_beam = element == Element::Rod || element == Element::Beam;
     let lin_geometry = kind.is_lin();
     if rod_or_beam && !lin_geometry {
@@ -29,50 +23,50 @@ fn get_cell_dofs(ndim: usize, element: Element, kind: GeoKind) -> Result<DofEidT
         return Err("GeoClass::Lin is reserved for Rod or Beam"); // inconsistent combination
     }
     let nnode = kind.nnode();
-    let mut total = 0; // local equation ID = total number of local equations in the end
-    let mut cell_dofs: Vec<Vec<(Dof, Eid)>> = vec![Vec::new(); nnode];
+    let mut dofs = vec![Vec::new(); nnode];
+    let mut count = 0;
     match element {
         Element::Rod => {
             for m in 0..nnode {
-                cell_dofs[m].push((Dof::Ux, total)); total += 1;
-                cell_dofs[m].push((Dof::Uy, total)); total += 1;
+                dofs[m].push((Dof::Ux, count)); count += 1;
+                dofs[m].push((Dof::Uy, count)); count += 1;
                 if ndim == 3 {
-                    cell_dofs[m].push((Dof::Uz, total)); total += 1;
+                    dofs[m].push((Dof::Uz, count)); count += 1;
                 }
             }
         }
         Element::Beam => {
             for m in 0..nnode {
-                cell_dofs[m].push((Dof::Ux, total)); total += 1;
-                cell_dofs[m].push((Dof::Uy, total)); total += 1;
+                dofs[m].push((Dof::Ux, count)); count += 1;
+                dofs[m].push((Dof::Uy, count)); count += 1;
                 if ndim == 2 {
-                    cell_dofs[m].push((Dof::Rz, total)); total += 1;
+                    dofs[m].push((Dof::Rz, count)); count += 1;
                 } else {
-                    cell_dofs[m].push((Dof::Uz, total)); total += 1;
-                    cell_dofs[m].push((Dof::Rx, total)); total += 1;
-                    cell_dofs[m].push((Dof::Ry, total)); total += 1;
-                    cell_dofs[m].push((Dof::Rz, total)); total += 1;
+                    dofs[m].push((Dof::Uz, count)); count += 1;
+                    dofs[m].push((Dof::Rx, count)); count += 1;
+                    dofs[m].push((Dof::Ry, count)); count += 1;
+                    dofs[m].push((Dof::Rz, count)); count += 1;
                 }
             }
         }
         Element::Solid => {
             for m in 0..nnode {
-                cell_dofs[m].push((Dof::Ux, total)); total += 1;
-                cell_dofs[m].push((Dof::Uy, total)); total += 1;
+                dofs[m].push((Dof::Ux, count)); count += 1;
+                dofs[m].push((Dof::Uy, count)); count += 1;
                 if ndim == 3 {
-                    cell_dofs[m].push((Dof::Uz, total)); total += 1;
+                    dofs[m].push((Dof::Uz, count)); count += 1;
                 }
             }
         }
         Element::PorousLiq => {
             for m in 0..nnode {
-                cell_dofs[m].push((Dof::Pl, total)); total += 1;
+                dofs[m].push((Dof::Pl, count)); count += 1;
             }
         }
         Element::PorousLiqGas => {
             for m in 0..nnode {
-                cell_dofs[m].push((Dof::Pl, total)); total += 1;
-                cell_dofs[m].push((Dof::Pg, total)); total += 1;
+                dofs[m].push((Dof::Pl, count)); count += 1;
+                dofs[m].push((Dof::Pg, count)); count += 1;
             }
         }
         Element::PorousSldLiq => {
@@ -80,15 +74,15 @@ fn get_cell_dofs(ndim: usize, element: Element, kind: GeoKind) -> Result<DofEidT
                 return Err("cannot set PorousSldLiq with given GeoKind");
             };
             for m in 0..nnode {
-                cell_dofs[m].push((Dof::Ux, total)); total += 1;
-                cell_dofs[m].push((Dof::Uy, total)); total += 1;
+                dofs[m].push((Dof::Ux, count)); count += 1;
+                dofs[m].push((Dof::Uy, count)); count += 1;
                 if ndim == 3 {
-                    cell_dofs[m].push((Dof::Uz, total)); total += 1;
+                    dofs[m].push((Dof::Uz, count)); count += 1;
                 }
             }
             let ncorner = kind.lower_order().unwrap().nnode();
             for m in 0..ncorner {
-                cell_dofs[m].push((Dof::Pl, total)); total += 1;
+                dofs[m].push((Dof::Pl, count)); count += 1;
             }
         }
         Element::PorousSldLiqGas => {
@@ -96,72 +90,89 @@ fn get_cell_dofs(ndim: usize, element: Element, kind: GeoKind) -> Result<DofEidT
                 return Err("cannot set PorousSldLiqGas with given GeoKind");
             };
             for m in 0..nnode {
-                cell_dofs[m].push((Dof::Ux, total)); total += 1;
-                cell_dofs[m].push((Dof::Uy, total)); total += 1;
+                dofs[m].push((Dof::Ux, count)); count += 1;
+                dofs[m].push((Dof::Uy, count)); count += 1;
                 if ndim == 3 {
-                    cell_dofs[m].push((Dof::Uz, total)); total += 1;
+                    dofs[m].push((Dof::Uz, count)); count += 1;
                 }
             }
             let ncorner = kind.lower_order().unwrap().nnode();
             for m in 0..ncorner {
-                cell_dofs[m].push((Dof::Pl, total)); total += 1;
-                cell_dofs[m].push((Dof::Pg, total)); total += 1;
+                dofs[m].push((Dof::Pl, count)); count += 1;
+                dofs[m].push((Dof::Pg, count)); count += 1;
             }
         }
     };
-    Ok((cell_dofs, total))
+    Ok((dofs, count))
 }
 
 pub struct DofNumbers {
-    point_dof_eid: Vec<HashMap<Dof, Eid>>,
-    local_to_global: Vec<Vec<Eid>>,
-    neq: usize,
+    /// Maps the combination of attribute ID and kind to the DOFs array
+    ///
+    /// * The DOFs array contains the DOF keys and numbers attached to every cell node.
+    /// * The second item in (ArrayDofNum, usize) is the total number of local DOFs.
+    cell_dofs: HashMap<(CellAttributeId, GeoKind), (ArrayDofNum, usize)>,
+
+    /// Holds all points DOFs and numbers
+    ///
+    /// * The array has a length equal to npoint
+    /// * The inner maps have variable lengths according to the number of DOFs at the point
+    point_dofs: Vec<HashMap<Dof, usize>>,
+
+    /// Holds all DOF numbers, organized in a per Cell fashion
+    ///
+    /// * The outer array has length equal to ncell
+    /// * The inner arrays have variable lengths according to the number of local DOFs of the cell
+    local_to_global: Vec<Vec<usize>>,
+
+    /// Holds the total number of DOFs
+    dof_count: usize,
 }
 
 impl DofNumbers {
     pub fn new(mesh: &Mesh, att_ele: &HashMap<CellAttributeId, Element>) -> Result<Self, StrError> {
         // auxiliary memoization data
         let npoint = mesh.points.len();
-        let mut memo_cell_dofs: HashMap<(CellAttributeId, GeoKind), DofEidTotal> = HashMap::new();
-        let mut memo_point_dofs: Vec<HashSet<Dof>> = vec![HashSet::new(); npoint];
+        let mut memo_point_dofs = vec![HashSet::new(); npoint];
 
-        // find the DOFs and local equation numbers of all cells
+        // find all cells' (DOFs, local numbers)
+        let mut cell_dofs = HashMap::new();
         for cell in &mesh.cells {
             let element = match att_ele.get(&cell.attribute_id) {
                 Some(e) => e,
                 None => return Err("cannot find attribute in att_ele map"),
             };
-            let (cell_dofs, _) = memo_cell_dofs
+            let (dofs, _) = cell_dofs
                 .entry((cell.attribute_id, cell.kind))
                 .or_insert(get_cell_dofs(mesh.ndim, *element, cell.kind)?);
             for m in 0..cell.points.len() {
-                for (dof, _) in &cell_dofs[m] {
+                for (dof, _) in &dofs[m] {
                     memo_point_dofs[cell.points[m]].insert(*dof);
                 }
             }
         }
 
         // compute all points' DOFs and global equation ids
-        let mut point_dof_eid: Vec<HashMap<Dof, Eid>> = vec![HashMap::new(); npoint];
-        let mut total = 0; // total number of global equations
+        let mut point_dofs: Vec<HashMap<Dof, usize>> = vec![HashMap::new(); npoint];
+        let mut dof_count = 0; // total number of DOFs
         for point_id in 0..npoint {
             let mut sorted_dofs: Vec<_> = memo_point_dofs[point_id].iter().collect();
             sorted_dofs.sort();
             for dof in sorted_dofs {
-                point_dof_eid[point_id].insert(*dof, total);
-                total += 1;
+                point_dofs[point_id].insert(*dof, dof_count);
+                dof_count += 1;
             }
         }
 
         // compute all cells' local_to_global maps
         let ncell = mesh.cells.len();
-        let mut local_to_global: Vec<Vec<Eid>> = vec![Vec::new(); ncell];
+        let mut local_to_global: Vec<Vec<usize>> = vec![Vec::new(); ncell];
         for cell in &mesh.cells {
-            let (cell_dofs, n_local_eqn) = memo_cell_dofs.get(&(cell.attribute_id, cell.kind)).unwrap();
+            let (cell_dofs, n_local_eqn) = cell_dofs.get(&(cell.attribute_id, cell.kind)).unwrap();
             local_to_global[cell.id] = vec![0; *n_local_eqn];
             for m in 0..cell.points.len() {
                 for (dof, local_eid) in &cell_dofs[m] {
-                    let global_eid = *point_dof_eid[cell.points[m]].get(dof).unwrap();
+                    let global_eid = *point_dofs[cell.points[m]].get(dof).unwrap();
                     local_to_global[cell.id][*local_eid] = global_eid;
                 }
             }
@@ -169,9 +180,10 @@ impl DofNumbers {
 
         // done
         Ok(DofNumbers {
-            point_dof_eid,
+            cell_dofs,
+            point_dofs,
             local_to_global,
-            neq: total,
+            dof_count,
         })
     }
 }
@@ -182,12 +194,12 @@ impl fmt::Display for DofNumbers {
         let thin_line = "--------------------------------------------------------------------------------\n";
 
         f.write_str(thick_line).unwrap();
-        write!(f, "number of equations = {}\n", self.neq).unwrap();
+        write!(f, "number of equations = {}\n", self.dof_count).unwrap();
 
         f.write_str(thin_line).unwrap();
         write!(f, "PointId: (Dof, EquationId)\n").unwrap();
-        for point_id in 0..self.point_dof_eid.len() {
-            let mut dof_eqn: Vec<_> = self.point_dof_eid[point_id].iter().collect();
+        for point_id in 0..self.point_dofs.len() {
+            let mut dof_eqn: Vec<_> = self.point_dofs[point_id].iter().collect();
             dof_eqn.sort_by(|a, b| a.0.partial_cmp(b.0).unwrap());
             write!(f, "{:?}: {:?}\n", point_id, dof_eqn).unwrap();
         }
