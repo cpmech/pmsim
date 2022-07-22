8e9dc3d2e96f003ee5b4e0a25631b6d86b6f0e35
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-07-22 18:53:30 +1000

    Add mesh with one Tri6

--- src/base/sample_meshes.rs
+++ src/base/sample_meshes.rs
@@ -5,6 +5,32 @@ use gemlab::shapes::GeoKind;
 pub struct SampleMeshes {}
 
 impl SampleMeshes {
+    /// Returns a mesh with one Tri6
+    ///       2
+    ///      / \
+    ///     /   \
+    ///    5     4
+    ///   /       \
+    ///  /         \
+    /// 0-----3-----1
+    #[rustfmt::skip]
+    pub fn one_tri6() -> Mesh {
+        Mesh {
+            ndim: 2,
+            points: vec![
+                Point { id: 0, coords: vec![0.0,  0.0  ] },
+                Point { id: 1, coords: vec![1.0,  0.0  ] },
+                Point { id: 2, coords: vec![0.5,  0.85 ] },
+                Point { id: 3, coords: vec![0.5,  0.0  ] },
+                Point { id: 4, coords: vec![0.75, 0.425] },
+                Point { id: 5, coords: vec![0.25, 0.425] },
+            ],
+            cells: vec![
+                Cell { id: 0, attribute_id: 1, kind: GeoKind::Tri6, points: vec![0, 1, 2, 3, 4, 5] },
+            ],
+        }
+    }
+
     /// Returns the mesh used in Example 1.4 of Bhatti's book
     ///
     /// ```text
@@ -51,11 +77,20 @@ impl SampleMeshes {
 #[cfg(test)]
 mod tests {
     use super::SampleMeshes;
-    use gemlab::mesh::draw_mesh;
+    use gemlab::mesh::{check_all, draw_mesh};
 
     #[test]
     fn sample_meshes_are_ok() {
+        let mesh = SampleMeshes::one_tri6();
+        check_all(&mesh).unwrap();
+        assert_eq!(mesh.points.len(), 6);
+        assert_eq!(mesh.cells.len(), 1);
+        if true {
+            draw_mesh(&mesh, true, "/tmp/pmsim/test_mesh_one_tri6.svg").unwrap();
+        }
+
         let mesh = SampleMeshes::bhatti_example_1dot4_truss();
+        check_all(&mesh).unwrap();
         assert_eq!(mesh.points.len(), 4);
         assert_eq!(mesh.cells.len(), 5);
         if false {
