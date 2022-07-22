f5569faf855433686462409d7ef7edbc9ebf16d4
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-07-22 17:39:39 +1000

    Impl CellDofInfo

--- src/base/dof_numbers.rs
+++ src/base/dof_numbers.rs
@@ -5,17 +5,52 @@ use gemlab::shapes::GeoKind;
 use std::collections::{HashMap, HashSet};
 use std::fmt;
 
-/// Defines a nested array with DOF keys and numbers
+/// Holds information about the Cell/Element DOFs
 ///
-/// * The outer array has length = npoint (or nnode)
-/// * The inner arrays have variable lengths = ndof at the point (or node)
-pub type ArrayDofNum = Vec<Vec<(Dof, usize)>>;
+/// # Examples
+///
+/// ```
+/// // leq: local equation number       leq   point   geq
+/// // geq: global equation number       ↓        ↓    ↓
+/// //                                   0 → Ux @ 0 →  0
+/// //            {Ux → 6}               1 → Uy @ 0 →  1
+/// //            {Uy → 7}               2 → Ux @ 1 →  3
+/// //            {Pl → 8}               3 → Uy @ 1 →  4
+/// //                2                  4 → Ux @ 2 →  6
+/// //               / \                 5 → Uy @ 2 →  7
+/// //   {Ux → 13}  /   \  {Ux → 11}     6 → Ux @ 3 →  9
+/// //   {Uy → 14} 5     4 {Uy → 12}     7 → Uy @ 3 → 10
+/// //            /       \              8 → Ux @ 4 → 11
+/// // {Ux → 0}  /         \  {Ux → 3}   9 → Uy @ 4 → 12
+/// // {Uy → 1} 0-----3-----1 {Uy → 4}  10 → Ux @ 5 → 13
+/// // {Pl → 2}   {Ux → 9}    {Pl → 5}  11 → Uy @ 5 → 14
+/// //            {Uy → 10}             12 → Pl @ 0 →  2  <<< start_lower_order
+/// //                                  13 → Pl @ 1 →  5
+/// //                                  14 → Pl @ 2 →  8
+/// ```
+pub struct CellDofInfo {
+    /// Holds all cell DOF keys and local equation numbers
+    ///
+    /// **Notes:** The outer array has length = nnode.
+    /// The inner arrays have variable lengths = ndof at the node.
+    pub dof_equation_pairs: Vec<Vec<(Dof, usize)>>,
 
-/// Returns the DOF keys and numbers for each cell node and the total number of local dofs
-///
-/// * The second item in (ArrayDofNum, usize) is the total number of local DOFs
+    /// Dimension of the local system of equations
+    ///
+    /// **Note:** This is equal to the total number of DOFs in the cell
+    pub n_equation_local: usize,
+
+    /// Local equation number of the first DOF at a "lower-order" node
+    ///
+    /// **Note:** If the element has mixed DOFs per node, e.g., "higher-order" nodes with [Ux, Uy],
+    /// and "lower-order" nodes with [Ux, Uy, Pl], this index marks the position of the first entry
+    /// in the local system of equations corresponding to the first mixed DOF, e.g., Pl.
+    pub eq_lo: Option<usize>,
+}
+
+/// Returns the DOF keys and local equation numbers for each cell node of (Element,GeoKind)
 #[rustfmt::skip]
-fn get_cell_dofs(ndim: usize, element: Element, kind: GeoKind) -> Result<(ArrayDofNum, usize), StrError> {
+fn get_cell_dofs(ndim: usize, element: Element, kind: GeoKind) -> Result<CellDofInfo, StrError> {
     let rod_or_beam = element == Element::Rod || element == Element::Beam;
     let lin_geometry = kind.is_lin();
     if rod_or_beam && !lin_geometry {
@@ -27,6 +62,7 @@ fn get_cell_dofs(ndim: usize, element: Element, kind: GeoKind) -> Result<(ArrayD
     let nnode = kind.nnode();
     let mut dofs = vec![Vec::new(); nnode];
     let mut count = 0;
+    let mut eq_lo = None;
     match element {
         Element::Rod => {
             for m in 0..nnode {
@@ -82,6 +118,7 @@ fn get_cell_dofs(ndim: usize, element: Element, kind: GeoKind) -> Result<(ArrayD
                     dofs[m].push((Dof::Uz, count)); count += 1;
                 }
             }
+            eq_lo = Some(count);
             let ncorner = kind.lower_order().unwrap().nnode();
             for m in 0..ncorner {
                 dofs[m].push((Dof::Pl, count)); count += 1;
@@ -98,6 +135,7 @@ fn get_cell_dofs(ndim: usize, element: Element, kind: GeoKind) -> Result<(ArrayD
                     dofs[m].push((Dof::Uz, count)); count += 1;
                 }
             }
+            eq_lo = Some(count);
             let ncorner = kind.lower_order().unwrap().nnode();
             for m in 0..ncorner {
                 dofs[m].push((Dof::Pl, count)); count += 1;
@@ -105,10 +143,24 @@ fn get_cell_dofs(ndim: usize, element: Element, kind: GeoKind) -> Result<(ArrayD
             }
         }
     };
-    Ok((dofs, count))
+    Ok(CellDofInfo {
+        dof_equation_pairs: dofs,
+        n_equation_local: count,
+        eq_lo,
+    })
 }
 
+/// Defines a nested array with DOF keys and numbers
+///
+/// * The outer array has length = npoint (or nnode)
+/// * The inner arrays have variable lengths = ndof at the point (or node)
+pub type ArrayDofNum = Vec<Vec<(Dof, usize)>>;
+
 pub struct DofNumbers {
+    /// Holds all combinations of attributes and shapes and associated
+    /// information about DOFs and local equation numbers
+    pub cell_dofs: HashMap<(CellAttributeId, GeoKind), CellDofInfo>,
+
     /// Holds all points DOFs and numbers
     ///
     /// **Notes:**
@@ -125,12 +177,15 @@ pub struct DofNumbers {
     /// 2. The inner arrays have variable lengths according to the number of local DOFs of the cell
     pub local_to_global: Vec<Vec<usize>>,
 
-    /// Holds the total number of DOFs
-    pub dof_count: usize,
+    /// Holds the total number of global equations
+    ///
+    /// **Note:** This is equal to the total number of DOFs
+    pub n_equation: usize,
 
     /// Holds the supremum of the number of nonzero values (nnz) in the global matrix
     ///
     /// **Notes:**
+    ///
     /// 1. The global matrix is sparse with the number of nonzero values indicated by `nnz`
     /// 2. The local element matrices add only to parts of the global matrix yielding a banded matrix
     /// 3. The largest upper bound of nnz is the total number of entries in the global matrix (nrow × ncol).
@@ -146,20 +201,20 @@ impl DofNumbers {
     pub fn new(mesh: &Mesh, att_ele: &HashMap<CellAttributeId, Element>) -> Result<Self, StrError> {
         // auxiliary memoization data
         let npoint = mesh.points.len();
-        let mut memo_cell_dofs: HashMap<(CellAttributeId, GeoKind), (ArrayDofNum, usize)> = HashMap::new();
         let mut memo_point_dofs = vec![HashSet::new(); npoint];
 
         // find all cell (DOFs, local numbers) pairs and add (unique) DOFs to the point DOFs array
+        let mut cell_dofs = HashMap::new();
         for cell in &mesh.cells {
             let element = match att_ele.get(&cell.attribute_id) {
                 Some(e) => e,
                 None => return Err("cannot find attribute in att_ele map"),
             };
-            let (dofs, _) = memo_cell_dofs
+            let info = cell_dofs
                 .entry((cell.attribute_id, cell.kind))
                 .or_insert(get_cell_dofs(mesh.ndim, *element, cell.kind)?);
             for m in 0..cell.points.len() {
-                for (dof, _) in &dofs[m] {
+                for (dof, _) in &info.dof_equation_pairs[m] {
                     memo_point_dofs[cell.points[m]].insert(*dof);
                 }
             }
@@ -167,13 +222,13 @@ impl DofNumbers {
 
         // compute all point DOF numbers
         let mut point_dofs = vec![HashMap::new(); npoint];
-        let mut dof_count = 0; // total number of DOFs
+        let mut n_equation = 0; // equals the total number of DOFs
         for point_id in 0..npoint {
             let mut sorted_dofs: Vec<_> = memo_point_dofs[point_id].iter().collect();
             sorted_dofs.sort();
             for dof in sorted_dofs {
-                point_dofs[point_id].insert(*dof, dof_count);
-                dof_count += 1;
+                point_dofs[point_id].insert(*dof, n_equation);
+                n_equation += 1;
             }
         }
 
@@ -182,22 +237,23 @@ impl DofNumbers {
         let mut local_to_global: Vec<Vec<usize>> = vec![Vec::new(); ncell];
         let mut nnz_sup = 0;
         for cell in &mesh.cells {
-            let (cell_dofs, ndof_local) = memo_cell_dofs.get(&(cell.attribute_id, cell.kind)).unwrap();
-            local_to_global[cell.id] = vec![0; *ndof_local];
+            let info = cell_dofs.get(&(cell.attribute_id, cell.kind)).unwrap();
+            local_to_global[cell.id] = vec![0; info.n_equation_local];
             for m in 0..cell.points.len() {
-                for (dof, local) in &cell_dofs[m] {
+                for (dof, local) in &info.dof_equation_pairs[m] {
                     let global = *point_dofs[cell.points[m]].get(dof).unwrap();
                     local_to_global[cell.id][*local] = global;
                 }
             }
-            nnz_sup += ndof_local * ndof_local;
+            nnz_sup += info.n_equation_local * info.n_equation_local;
         }
 
         // done
         Ok(DofNumbers {
+            cell_dofs,
             point_dofs,
             local_to_global,
-            dof_count,
+            n_equation,
             nnz_sup,
         })
     }
@@ -205,28 +261,37 @@ impl DofNumbers {
 
 impl fmt::Display for DofNumbers {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let thick_line = "================================================================================\n";
-        let thin_line = "--------------------------------------------------------------------------------\n";
+        write!(f, "\nCells: DOFs and local equation numbers\n").unwrap();
+        write!(f, "======================================\n").unwrap();
+        let mut keys: Vec<_> = self.cell_dofs.keys().collect();
+        keys.sort_by(|a, b| a.0.cmp(&b.0));
+        for key in keys {
+            let info = self.cell_dofs.get(key).unwrap();
+            write!(f, "{} {:?} eq_lo = {:?}\n", key.0, key.1, info.eq_lo).unwrap();
+            for m in 0..info.dof_equation_pairs.len() {
+                write!(f, "    {}: {:?}\n", m, info.dof_equation_pairs[m]).unwrap();
+            }
+        }
 
-        f.write_str(thick_line).unwrap();
-        write!(f, "total number of DOFs (equations) = {}\n", self.dof_count).unwrap();
-        write!(f, "supremum of the number of non-zeros (nnz_sup) = {}\n", self.nnz_sup).unwrap();
-
-        f.write_str(thin_line).unwrap();
-        write!(f, "PointId: (DOF, number)\n").unwrap();
+        write!(f, "\nPoints: DOFs and global equation numbers\n").unwrap();
+        write!(f, "========================================\n").unwrap();
         for point_id in 0..self.point_dofs.len() {
             let mut dof_eqn: Vec<_> = self.point_dofs[point_id].iter().collect();
             dof_eqn.sort_by(|a, b| a.0.partial_cmp(b.0).unwrap());
             write!(f, "{:?}: {:?}\n", point_id, dof_eqn).unwrap();
         }
 
-        f.write_str(thin_line).unwrap();
-        write!(f, "CellId: Local-to-Global\n").unwrap();
+        write!(f, "\nCells: Local-to-Global\n").unwrap();
+        write!(f, "======================\n").unwrap();
         for cell_id in 0..self.local_to_global.len() {
             write!(f, "{:?}: {:?}\n", cell_id, self.local_to_global[cell_id]).unwrap();
         }
 
-        f.write_str(thick_line)
+        write!(f, "\nInformation\n").unwrap();
+        write!(f, "===========\n").unwrap();
+        write!(f, "total number of DOFs (equations) = {}\n", self.n_equation).unwrap();
+        write!(f, "supremum of the number of non-zeros (nnz_sup) = {}\n", self.nnz_sup).unwrap();
+        Ok(())
     }
 }
 
@@ -251,6 +316,8 @@ mod tests {
         let mesh = Samples::qua8_tri6_lin2();
         let att_ele = HashMap::from([(1, Element::PorousSldLiq), (2, Element::Solid), (3, Element::Beam)]);
         let dn = DofNumbers::new(&mesh, &att_ele)?;
+
+        println!("{}", dn);
 
         // check point dofs
         assert_point_dofs(&dn, 0, &[(Dof::Ux, 0), (Dof::Uy, 1), (Dof::Pl, 2)]);
@@ -279,7 +346,7 @@ mod tests {
         let nnz_porous_qua8 = (ndim * 8 + 4) * (ndim * 8 + 4);
         let nnz_solid_tri6 = (ndim * 6) * (ndim * 6);
         let nnz_beam = (3 * 2) * (3 * 2);
-        assert_eq!(dn.dof_count, 29);
+        assert_eq!(dn.n_equation, 29);
         assert_eq!(dn.nnz_sup, nnz_porous_qua8 + nnz_solid_tri6 + 2 * nnz_beam);
         Ok(())
     }
