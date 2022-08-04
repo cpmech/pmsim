d4ba282091cc17696b062afd89d9d4e0c028f9df
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-08-04 22:26:40 +1000

    Disable rod element

--- src/elements/mod.rs
+++ src/elements/mod.rs
@@ -1,8 +1,8 @@
 //! Implements the finite elements
 
 // mod porous_sld_liq;
-mod rod;
+// mod rod;
 // mod solid;
 // pub use crate::elements::porous_sld_liq::*;
-pub use crate::elements::rod::*;
+// pub use crate::elements::rod::*;
 // pub use crate::elements::solid::*;
