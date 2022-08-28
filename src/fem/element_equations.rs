563c9628ccbf2623d33614ab05cc021e75fbf71a
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-08-28 16:40:58 +1000

    Add tests

--- src/fem/element_equations.rs
+++ src/fem/element_equations.rs
@@ -3,11 +3,13 @@ use crate::base::{Config, DofNumbers, Element};
 use crate::StrError;
 use gemlab::mesh::{Cell, Mesh};
 
+/// Defines the trait for element equations
 pub trait ElementEquations {
     fn residual(&mut self) -> Result<(), StrError>;
     fn jacobian(&mut self) -> Result<(), StrError>;
 }
 
+/// Allocates element equations
 pub fn allocate_element_equations(
     mesh: &Mesh,
     dn: &DofNumbers,
@@ -28,4 +30,24 @@ pub fn allocate_element_equations(
         Element::PorousSldLiqGas(..) => panic!("TODO"),
     };
     Ok(element_equations)
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::allocate_element_equations;
+    use crate::base::{Config, DofNumbers, Element, SampleParams};
+    use gemlab::mesh::Samples;
+    use std::collections::HashMap;
+
+    #[test]
+    fn allocate_element_equations_works() {
+        let mesh = Samples::one_tri3();
+        let p1 = SampleParams::param_solid();
+        let elements = HashMap::from([(1, Element::Solid(p1))]);
+        let dn = DofNumbers::new(&mesh, elements).unwrap();
+        let config = Config::new();
+        allocate_element_equations(&mesh, &dn, &config, &mesh.cells[0]).unwrap();
+    }
 }
--- src/fem/simulation.rs
+++ src/fem/simulation.rs
@@ -1,10 +1,11 @@
-use super::{allocate_element_equations, ElementEquations};
+use super::{allocate_element_equations, ElementEquations, State};
 use crate::base::{Config, DofNumbers};
 use crate::StrError;
 use gemlab::mesh::Mesh;
 
 pub struct Simulation {
     pub element_equations: Vec<Box<dyn ElementEquations>>, // (ncell)
+    pub state: State,
 }
 
 impl Simulation {
@@ -14,9 +15,35 @@ impl Simulation {
             .iter()
             .map(|cell| allocate_element_equations(mesh, dn, config, cell))
             .collect();
-        match res {
-            Ok(v) => Ok(Simulation { element_equations: v }),
-            Err(e) => Err(e),
-        }
+        let element_equations = match res {
+            Ok(v) => v,
+            Err(e) => return Err(e),
+        };
+        Ok(Simulation {
+            element_equations,
+            state: State::new(mesh, dn, config)?,
+        })
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::Simulation;
+    use crate::base::{Config, DofNumbers, Element, SampleParams};
+    use gemlab::mesh::Samples;
+    use std::collections::HashMap;
+
+    #[test]
+    fn new_works() {
+        let mesh = Samples::one_tri3();
+        let p1 = SampleParams::param_solid();
+        let elements = HashMap::from([(1, Element::Solid(p1))]);
+        let dn = DofNumbers::new(&mesh, elements).unwrap();
+        let config = Config::new();
+        let sim = Simulation::new(&mesh, &dn, &config).unwrap();
+        assert_eq!(sim.element_equations.len(), 1);
+        assert_eq!(sim.state.effective_stress.len(), 1);
     }
 }
