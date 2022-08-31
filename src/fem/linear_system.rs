c88baad2e1f9118bcd5ad450c2607d20986dfc82
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-08-31 23:43:03 +1000

    Rename Solution to LinearSystem

--- src/fem/linear_system.rs
+++ src/fem/linear_system.rs
@@ -1,16 +1,14 @@
 use russell_lab::Vector;
 use russell_sparse::{SparseTriplet, Symmetry};
 
-pub struct Solution {
-    pub dt: f64,
+pub struct LinearSystem {
     pub residual: Vector,
     pub jacobian: SparseTriplet,
 }
 
-impl Solution {
+impl LinearSystem {
     pub fn new(neq: usize, nnz: usize) -> Self {
-        Solution {
-            dt: 0.0,
+        LinearSystem {
             residual: Vector::new(neq),
             jacobian: SparseTriplet::new(neq, neq, nnz, Symmetry::No).unwrap(),
         }
--- src/fem/mod.rs
+++ src/fem/mod.rs
@@ -1,12 +1,16 @@
 //! Implements the finite element method
 
 mod element_collection;
+mod element_diffusion;
 mod element_equations;
 mod element_rod;
 mod element_solid;
+mod linear_system;
 mod state;
 pub use crate::fem::element_collection::*;
+pub use crate::fem::element_diffusion::*;
 pub use crate::fem::element_equations::*;
 pub use crate::fem::element_rod::*;
 pub use crate::fem::element_solid::*;
+pub use crate::fem::linear_system::*;
 pub use crate::fem::state::*;
