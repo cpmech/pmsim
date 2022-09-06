0532a8bdb32512fb35ffb71f618309af236f3842
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-09-06 21:36:17 +1000

    Improve test

--- examples/gen_mesh_heat_transient_1d.rs
+++ examples/gen_mesh_heat_transient_1d.rs
@@ -3,8 +3,8 @@ use gemlab::{prelude::*, StrError};
 fn main() -> Result<(), StrError> {
     let mut block = Block::new(&[[0.0, 0.0], [20.0, 0.0], [20.0, 1.0], [0.0, 1.0]])?;
     block.set_ndiv(&[10, 1])?;
-    let mesh = block.subdivide(GeoKind::Qua4)?;
-    mesh.write("/tmp/pmsim/mesh_heat_transient_1d.dat")?;
-    draw_mesh(&mesh, true, "/tmp/pmsim/mesh_heat_transient_1d.svg")?;
+    let mesh = block.subdivide(GeoKind::Qua8)?;
+    mesh.write("/tmp/pmsim/mesh_heat_transient_1d_qua8.dat")?;
+    draw_mesh(&mesh, true, "/tmp/pmsim/mesh_heat_transient_1d_qua8.svg")?;
     Ok(())
 }
--- tests/test_heat_transient_1d.rs
+++ tests/test_heat_transient_1d.rs
@@ -1,10 +1,12 @@
 use gemlab::prelude::*;
 use pmsim::{fem::sim_transient, prelude::*, StrError};
+use russell_lab::math::{erfc, PI};
 
 #[test]
 fn test_heat_transient_1d() -> Result<(), StrError> {
     // mesh and boundary features
-    let mesh = Mesh::read("data/meshes/mesh_heat_transient_1d.dat")?;
+    // let mesh = Mesh::read("data/meshes/mesh_heat_transient_1d.dat")?;
+    let mesh = Mesh::read("data/meshes/mesh_heat_transient_1d_qua8.dat")?;
     let find = Find::new(&mesh, None);
     let left = find.edges(At::X(0.0))?;
 
@@ -46,5 +48,27 @@ fn test_heat_transient_1d() -> Result<(), StrError> {
         &mut state,
         &mut lin_sys,
         &config,
-    )
+    )?;
+
+    // check
+    let analytical = |t: f64, x: f64| {
+        2.0 * f64::sqrt(t / PI)
+            * (f64::exp(-x * x / (4.0 * t)) - (x / 2.0) * f64::sqrt(PI / t) * erfc(x / (2.0 * f64::sqrt(t))))
+    };
+    let selected = vec![
+        find.point_ids(At::X(0.0)).unwrap(),
+        find.point_ids(At::X(1.0)).unwrap(),
+        find.point_ids(At::X(2.0)).unwrap(),
+    ]
+    .concat();
+    println!("");
+    for p in &selected {
+        let x = data.mesh.points[*p].coords[0];
+        let eq = data.equations.eq(*p, Dof::T).unwrap();
+        let tt = state.uu[eq];
+        let diff = f64::abs(tt - analytical(state.t, x));
+        println!("point = {}, x = {:.2}, tt = {:.6}, diff = {:.4e}", p, x, tt, diff);
+        assert!(diff < 3e-2);
+    }
+    Ok(())
 }
