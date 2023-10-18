48d62293a8aa32c2fcde695a735902a67113a4aa
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-10-19 06:22:21 +1000

    Rename pressurized_cylinder_plot

--- zscripts/run-pressurized-cylinder2d.bash
+++ zscripts/run-pressurized-cylinder2d.bash
@@ -24,5 +24,5 @@ for genie in $GENIES; do
     for kind in $KINDS; do
         cargo run --release --example pressurized_cylinder2d_elast -- $genie $kind
     done
-    cargo run --release --example plot_convergence_results -- pressurized_cylinder2d_elast $genie
+    cargo run --release --example pressurized_cylinder_plot -- pressurized_cylinder2d_elast $genie
 done
