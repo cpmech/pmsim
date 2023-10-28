30741f3fdc10ec21335a7d25b3c5e766d620b15c
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-10-28 12:38:37 +1000

    Improve script

--- zscripts/run-all-examples.bash
+++ zscripts/run-all-examples.bash
@@ -2,8 +2,14 @@
 
 set -e
 
+cargo build
+
 for ex in examples/*.rs; do
     key=`basename -s ".rs" $ex`
+    echo
+    echo
+    echo
+    echo "###### $key ################################################"
     if [ "$key" = "pressurized_cylinder_plot" ] || 
        [ "$key" = "pressurized_cylinder_table" ] || 
        [ "$key" = "pressurized_cylinder2d_elast" ] || 
