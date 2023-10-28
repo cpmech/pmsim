656524f0d63fea9b65321ee9321bf1dd48194969
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-10-28 10:31:46 +1000

    Rename fem_input file

--- src/fem/mod.rs
+++ src/fem/mod.rs
@@ -2,12 +2,12 @@
 
 mod boundaries;
 mod concentrated_loads;
-mod data;
 mod element_diffusion;
 mod element_rod;
 mod element_solid;
 mod element_trait;
 mod elements;
+mod fem_input;
 mod linear_system;
 mod post_processing;
 mod prescribed_values;
@@ -15,12 +15,12 @@ mod simulation;
 mod state;
 pub use crate::fem::boundaries::*;
 pub use crate::fem::concentrated_loads::*;
-pub use crate::fem::data::*;
 pub use crate::fem::element_diffusion::*;
 pub use crate::fem::element_rod::*;
 pub use crate::fem::element_solid::*;
 pub use crate::fem::element_trait::*;
 pub use crate::fem::elements::*;
+pub use crate::fem::fem_input::*;
 pub use crate::fem::linear_system::*;
 pub use crate::fem::post_processing::*;
 pub use crate::fem::prescribed_values::*;
