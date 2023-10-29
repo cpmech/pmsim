5a9e694deb9681e10e5920ff50773a38138a9d1f
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-10-30 08:25:52 +1000

    Use t4_ddot_t2_update in linear elastic model

--- src/material/linear_elastic.rs
+++ src/material/linear_elastic.rs
@@ -1,19 +1,16 @@
 use super::StressState;
 use super::StressStrainModel;
-use crate::base::new_tensor2;
 use crate::StrError;
-use russell_tensor::{copy_tensor4, t4_ddot_t2, LinElasticity, Tensor2, Tensor4};
+use russell_tensor::{copy_tensor4, t4_ddot_t2_update, LinElasticity, Tensor2, Tensor4};
 
 pub struct LinearElastic {
     pub model: LinElasticity,
-    pub dsigma: Tensor2,
 }
 
 impl LinearElastic {
     pub fn new(young: f64, poisson: f64, two_dim: bool, plane_stress: bool) -> Self {
         LinearElastic {
             model: LinElasticity::new(young, poisson, two_dim, plane_stress),
-            dsigma: new_tensor2(two_dim),
         }
     }
 }
@@ -35,7 +32,6 @@ impl StressStrainModel for LinearElastic {
 
     fn update_stress(&mut self, state: &mut StressState, deps: &Tensor2) -> Result<(), StrError> {
         let dd = self.model.get_modulus();
-        t4_ddot_t2(&mut self.dsigma, 1.0, dd, deps)?; // Δσ = D : Δε
-        state.sigma.add(1.0, &self.dsigma)
+        t4_ddot_t2_update(&mut state.sigma, 1.0, dd, deps, 1.0) // σ += D : Δε
     }
 }
