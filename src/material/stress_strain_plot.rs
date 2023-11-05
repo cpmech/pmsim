d931ba94d0a929dee25e49837bd7b128a3ffb144
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-11-05 20:44:38 +1000

    Improve stress strain plot

--- src/material/stress_strain_plot.rs
+++ src/material/stress_strain_plot.rs
@@ -227,6 +227,44 @@ impl StressStrainPlot {
         };
         Ok(())
     }
+
+    /// Plots the vol(strain)-dev(strain) invariants curve
+    pub fn vol_strain_dev_strain(
+        &mut self,
+        stresses: &Vec<Tensor2>,
+        strains: &Vec<Tensor2>,
+        params: Option<SSPlotParams>,
+    ) -> Result<(), StrError> {
+        if stresses.len() != strains.len() {
+            return Err("arrays of stresses and strains must have the same length");
+        }
+        let p = match params {
+            Some(v) => v,
+            None => SSPlotParams::new(),
+        };
+        let x: Vec<_> = if p.percentage_strains {
+            strains.iter().map(|eps| 100.0 * eps.invariant_eps_d()).collect()
+        } else {
+            strains.iter().map(|eps| eps.invariant_eps_d()).collect()
+        };
+        let y: Vec<_> = if p.percentage_strains {
+            strains.iter().map(|eps| 100.0 * eps.invariant_eps_v()).collect()
+        } else {
+            strains.iter().map(|eps| eps.invariant_eps_v()).collect()
+        };
+        self.curve_vol_strain_dev_strain.draw(&x, &y);
+        self.curve_vol_strain_dev_strain_x_label = if p.percentage_strains {
+            "$\\varepsilon_d$ [%]".to_string()
+        } else {
+            "$\\varepsilon_d$".to_string()
+        };
+        self.curve_vol_strain_dev_strain_y_label = if p.percentage_strains {
+            "$\\varepsilon_v$ [%]".to_string()
+        } else {
+            "$\\varepsilon_v$".to_string()
+        };
+        Ok(())
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -342,6 +380,41 @@ mod tests {
             .unwrap();
         if SAVE_FIGURE {
             plot.save_dev_stress_vol_strain("/tmp/pmsim/test_dev_stress_vol_strain_3.svg")
+                .unwrap();
+        }
+    }
+
+    #[test]
+    pub fn vol_strain_dev_strain_works() {
+        let young = 1500.0;
+        let poisson = 0.25;
+        let mut path = StressStrainPath::new(young, poisson, true);
+        let dsigma_m = 1.0;
+        let dsigma_d = 9.0;
+        let lode = 1.0;
+        for i in 0..4 {
+            let m = (i + 1) as f64;
+            let sigma_m = m * dsigma_m;
+            let sigma_d = m * dsigma_d;
+            path.push_stress_oct(sigma_m, sigma_d, lode, true).unwrap();
+        }
+
+        let mut plot = StressStrainPlot::new();
+        plot.curve_vol_strain_dev_strain.set_line_color("red");
+        plot.vol_strain_dev_strain(&path.stresses, &path.strains, None).unwrap();
+        if SAVE_FIGURE {
+            plot.save_vol_strain_dev_strain("/tmp/pmsim/test_vol_strain_dev_strain_1.svg")
+                .unwrap();
+        }
+
+        let mut params = SSPlotParams::new();
+        params.percentage_strains = true;
+        let mut plot = StressStrainPlot::new();
+        plot.curve_vol_strain_dev_strain.set_marker_style("o");
+        plot.vol_strain_dev_strain(&path.stresses, &path.strains, Some(params))
+            .unwrap();
+        if SAVE_FIGURE {
+            plot.save_vol_strain_dev_strain("/tmp/pmsim/test_vol_strain_dev_strain_2.svg")
                 .unwrap();
         }
     }
