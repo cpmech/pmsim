23e2ab53c5faa07d27409cd0df9ae75f38bd8996
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-11-05 20:40:23 +1000

    Improve stress strain plot

--- src/material/stress_strain_plot.rs
+++ src/material/stress_strain_plot.rs
@@ -16,6 +16,20 @@ pub struct StressStrainPlot {
     pub curve_dev_stress_dev_strain: Curve,
     curve_dev_stress_dev_strain_x_label: String,
     curve_dev_stress_dev_strain_y_label: String,
+
+    pub curve_dev_stress_vol_strain: Curve,
+    curve_dev_stress_vol_strain_x_label: String,
+    curve_dev_stress_vol_strain_y_label: String,
+
+    pub curve_vol_strain_dev_strain: Curve,
+    curve_vol_strain_dev_strain_x_label: String,
+    curve_vol_strain_dev_strain_y_label: String,
+
+    pub curve_dev_mean_stress_path: Curve,
+    curve_dev_mean_stress_path_x_label: String,
+    curve_dev_mean_stress_path_y_label: String,
+
+    pub curve_octahedral_stress_path: Curve,
 }
 
 impl SSPlotParams {
@@ -36,18 +50,24 @@ impl StressStrainPlot {
             curve_dev_stress_dev_strain: Curve::new(),
             curve_dev_stress_dev_strain_x_label: String::new(),
             curve_dev_stress_dev_strain_y_label: String::new(),
+
+            curve_dev_stress_vol_strain: Curve::new(),
+            curve_dev_stress_vol_strain_x_label: String::new(),
+            curve_dev_stress_vol_strain_y_label: String::new(),
+
+            curve_vol_strain_dev_strain: Curve::new(),
+            curve_vol_strain_dev_strain_x_label: String::new(),
+            curve_vol_strain_dev_strain_y_label: String::new(),
+
+            curve_dev_mean_stress_path: Curve::new(),
+            curve_dev_mean_stress_path_x_label: String::new(),
+            curve_dev_mean_stress_path_y_label: String::new(),
+
+            curve_octahedral_stress_path: Curve::new(),
         }
     }
 
     /// Saves the dev(stress)-dev(strain) curve
-    ///
-    /// # Input
-    ///
-    /// * `figure_path` -- may be a String, &str, or Path
-    ///
-    /// # Note
-    ///
-    /// Call `set_show_errors` to configure how the errors (if any) are printed.
     pub fn save_dev_stress_dev_strain<S>(&self, figure_path: &S) -> Result<(), StrError>
     where
         S: AsRef<OsStr> + ?Sized,
@@ -61,6 +81,58 @@ impl StressStrainPlot {
             .save(figure_path)
     }
 
+    /// Saves the dev(stress)-vol(strain) curve
+    pub fn save_dev_stress_vol_strain<S>(&self, figure_path: &S) -> Result<(), StrError>
+    where
+        S: AsRef<OsStr> + ?Sized,
+    {
+        let mut plot = Plot::new();
+        plot.add(&self.curve_dev_stress_vol_strain)
+            .grid_and_labels(
+                &self.curve_dev_stress_vol_strain_x_label,
+                &self.curve_dev_stress_vol_strain_y_label,
+            )
+            .save(figure_path)
+    }
+
+    /// Saves the vol(strain)-dev(strain) curve
+    pub fn save_vol_strain_dev_strain<S>(&self, figure_path: &S) -> Result<(), StrError>
+    where
+        S: AsRef<OsStr> + ?Sized,
+    {
+        let mut plot = Plot::new();
+        plot.add(&self.curve_vol_strain_dev_strain)
+            .grid_and_labels(
+                &self.curve_vol_strain_dev_strain_x_label,
+                &self.curve_vol_strain_dev_strain_y_label,
+            )
+            .save(figure_path)
+    }
+
+    /// Saves the dev(stress)-mean(stress) path
+    pub fn save_dev_mean_stress_path<S>(&self, figure_path: &S) -> Result<(), StrError>
+    where
+        S: AsRef<OsStr> + ?Sized,
+    {
+        let mut plot = Plot::new();
+        plot.add(&self.curve_dev_mean_stress_path)
+            .grid_and_labels(
+                &self.curve_dev_mean_stress_path_x_label,
+                &self.curve_dev_mean_stress_path_y_label,
+            )
+            .save(figure_path)
+    }
+
+    /// Saves the octahedral stress path
+    pub fn save_octahedral_stress_path<S>(&self, figure_path: &S) -> Result<(), StrError>
+    where
+        S: AsRef<OsStr> + ?Sized,
+    {
+        let mut plot = Plot::new();
+        plot.add(&self.curve_octahedral_stress_path).save(figure_path)
+    }
+
+    /// Plots the dev(stress)-dev(strain) invariants curve
     pub fn dev_stress_dev_strain(
         &mut self,
         stresses: &Vec<Tensor2>,
@@ -107,6 +179,54 @@ impl StressStrainPlot {
         };
         Ok(())
     }
+
+    /// Plots the dev(stress)-vol(strain) invariants curve
+    pub fn dev_stress_vol_strain(
+        &mut self,
+        stresses: &Vec<Tensor2>,
+        strains: &Vec<Tensor2>,
+        params: Option<SSPlotParams>,
+    ) -> Result<(), StrError> {
+        if stresses.len() != strains.len() {
+            return Err("arrays of stresses and strains must have the same length");
+        }
+        let p = match params {
+            Some(v) => v,
+            None => SSPlotParams::new(),
+        };
+        let x: Vec<_> = if p.percentage_strains {
+            strains.iter().map(|eps| 100.0 * eps.invariant_eps_v()).collect()
+        } else {
+            strains.iter().map(|eps| eps.invariant_eps_v()).collect()
+        };
+        let y: Vec<_> = if p.divide_by_sigma_m {
+            stresses
+                .iter()
+                .map(|sig| {
+                    let den = if p.negative_sigma_m {
+                        -sig.invariant_sigma_m()
+                    } else {
+                        sig.invariant_sigma_m()
+                    };
+                    sig.invariant_sigma_d() / den
+                })
+                .collect()
+        } else {
+            stresses.iter().map(|sig| sig.invariant_sigma_d()).collect()
+        };
+        self.curve_dev_stress_vol_strain.draw(&x, &y);
+        self.curve_dev_stress_vol_strain_x_label = if p.percentage_strains {
+            "$\\varepsilon_v$ [%]".to_string()
+        } else {
+            "$\\varepsilon_v$".to_string()
+        };
+        self.curve_dev_stress_vol_strain_y_label = if p.divide_by_sigma_m {
+            format!("${}\\sigma_d / \\sigma_m$", if p.negative_sigma_m { "-" } else { "" })
+        } else {
+            "$\\sigma_d$".to_string()
+        };
+        Ok(())
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -131,7 +251,7 @@ mod tests {
     }
 
     #[test]
-    pub fn stress_strain_plot_works() {
+    pub fn dev_stress_dev_strain_works() {
         let young = 1500.0;
         let poisson = 0.25;
         let mut path = StressStrainPath::new(young, poisson, true);
@@ -146,6 +266,7 @@ mod tests {
         }
 
         let mut plot = StressStrainPlot::new();
+        plot.curve_dev_stress_dev_strain.set_line_color("red");
         plot.dev_stress_dev_strain(&path.stresses, &path.strains, None).unwrap();
         if SAVE_FIGURE {
             plot.save_dev_stress_dev_strain("/tmp/pmsim/test_dev_stress_dev_strain_1.svg")
@@ -156,6 +277,7 @@ mod tests {
         params.percentage_strains = true;
         params.divide_by_sigma_m = true;
         let mut plot = StressStrainPlot::new();
+        plot.curve_dev_stress_dev_strain.set_marker_style("o");
         plot.dev_stress_dev_strain(&path.stresses, &path.strains, Some(params))
             .unwrap();
         if SAVE_FIGURE {
@@ -172,6 +294,54 @@ mod tests {
             .unwrap();
         if SAVE_FIGURE {
             plot.save_dev_stress_dev_strain("/tmp/pmsim/test_dev_stress_dev_strain_3.svg")
+                .unwrap();
+        }
+    }
+
+    #[test]
+    pub fn dev_stress_vol_strain_works() {
+        let young = 1500.0;
+        let poisson = 0.25;
+        let mut path = StressStrainPath::new(young, poisson, true);
+        let dsigma_m = 1.0;
+        let dsigma_d = 9.0;
+        let lode = 1.0;
+        for i in 0..4 {
+            let m = (i + 1) as f64;
+            let sigma_m = m * dsigma_m;
+            let sigma_d = m * dsigma_d;
+            path.push_stress_oct(sigma_m, sigma_d, lode, true).unwrap();
+        }
+
+        let mut plot = StressStrainPlot::new();
+        plot.curve_dev_stress_vol_strain.set_line_color("red");
+        plot.dev_stress_vol_strain(&path.stresses, &path.strains, None).unwrap();
+        if SAVE_FIGURE {
+            plot.save_dev_stress_vol_strain("/tmp/pmsim/test_dev_stress_vol_strain_1.svg")
+                .unwrap();
+        }
+
+        let mut params = SSPlotParams::new();
+        params.percentage_strains = true;
+        params.divide_by_sigma_m = true;
+        let mut plot = StressStrainPlot::new();
+        plot.curve_dev_stress_vol_strain.set_marker_style("o");
+        plot.dev_stress_vol_strain(&path.stresses, &path.strains, Some(params))
+            .unwrap();
+        if SAVE_FIGURE {
+            plot.save_dev_stress_vol_strain("/tmp/pmsim/test_dev_stress_vol_strain_2.svg")
+                .unwrap();
+        }
+
+        let mut params = SSPlotParams::new();
+        params.percentage_strains = true;
+        params.divide_by_sigma_m = true;
+        params.negative_sigma_m = true;
+        let mut plot = StressStrainPlot::new();
+        plot.dev_stress_vol_strain(&path.stresses, &path.strains, Some(params))
+            .unwrap();
+        if SAVE_FIGURE {
+            plot.save_dev_stress_vol_strain("/tmp/pmsim/test_dev_stress_vol_strain_3.svg")
                 .unwrap();
         }
     }
