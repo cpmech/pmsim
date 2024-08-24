7a0ed982afaa8dce15c056da4750ccd82c6c2e00
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-08-24 11:51:44 +1000

    Rename variable

--- src/material/local_state.rs
+++ src/material/local_state.rs
@@ -1,5 +1,6 @@
 use russell_lab::Vector;
 use russell_tensor::{Mandel, Tensor2};
+use serde::{Deserialize, Serialize};
 
 /// Constant to indicate an uninitialized value
 pub(crate) const UNINITIALIZED: f64 = f64::INFINITY;
@@ -7,6 +8,28 @@ pub(crate) const UNINITIALIZED: f64 = f64::INFINITY;
 /// Holds local state data for FEM simulations of porous materials
 ///
 /// This data is associated with a Gauss (integration) point
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct LocalState {
+    /// Holds the internal values Z
+    pub internal_values: Vector,
+
+    /// Holds the stress tensor σ
+    pub stress: Tensor2,
+
+    /// Holds the elastic (vs elastoplastic) flag
+    pub elastic: bool,
+
+    /// Holds the apex return flag for implicit methods
+    pub apex_return: bool,
+
+    /// Holds the algorithmic lagrange multiplier (Λ) for implicit methods
+    pub algo_lagrange: f64,
+}
+
+/// Holds local state data for FEM simulations of porous materials
+///
+/// This data is associated with a Gauss (integration) point
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct LocalStatePorous {
     /// Holds the internal values Z
     pub internal_values: Vector,
@@ -29,11 +52,12 @@ pub struct LocalStatePorous {
     /// Holds the porosity
     pub porosity: f64,
 
-    /// Holds the wetting (vs drying) flag
-    pub wetting: bool,
+    /// Holds the drying (vs wetting) flag
+    pub drying: bool,
 }
 
 /// Holds state data for generating stress-strain paths
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct LocalStatePath {
     /// Holds the stress tensor σ
     pub stress: Tensor2,
@@ -43,11 +67,13 @@ pub struct LocalStatePath {
 }
 
 /// Implements an array of LocalStatePath
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ArrLocalStatePath {
     pub all: Vec<LocalStatePath>,
 }
 
 /// Holds all state data (e.g., for plotting results)
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct LocalStateAll {
     /// Holds the internal values Z
     pub internal_values: Vector,
@@ -72,8 +98,21 @@ pub struct LocalStateAll {
 }
 
 /// Implements an array of LocalStateAll
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ArrLocalStateAll {
     pub all: Vec<LocalStateAll>,
+}
+
+impl LocalState {
+    pub fn new(mandel: Mandel, n_internal_values: usize) -> Self {
+        LocalState {
+            internal_values: Vector::new(n_internal_values),
+            stress: Tensor2::new(mandel),
+            elastic: true,
+            apex_return: false,
+            algo_lagrange: 0.0,
+        }
+    }
 }
 
 impl LocalStatePath {
