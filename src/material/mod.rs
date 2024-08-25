292ec46a182d9a57a7ce8e3ba86a4ebcd4877593
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-08-26 09:44:08 +1000

    Rename model files

--- src/material/mod.rs
+++ src/material/mod.rs
@@ -1,12 +1,12 @@
 //! Implements material models
 
-mod conductivity;
 mod linear_elastic;
 mod local_state;
-mod stress_strain_model;
+mod model_conductivity;
+mod model_stress_strain;
 mod von_mises;
-pub use crate::material::conductivity::*;
 pub use crate::material::linear_elastic::*;
 pub use crate::material::local_state::*;
-pub use crate::material::stress_strain_model::*;
+pub use crate::material::model_conductivity::*;
+pub use crate::material::model_stress_strain::*;
 pub use crate::material::von_mises::*;
