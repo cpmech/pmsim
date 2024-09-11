7279346cf17e7652911b698e1e7bcd616795faa1
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-09-11 17:40:01 +1000

    Rename file

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,8 +3,8 @@
 mod convergence_results;
 mod paraview;
 mod reference_data;
-mod validate_results;
+mod verify_results;
 pub use crate::util::convergence_results::*;
 pub use crate::util::paraview::*;
 pub use crate::util::reference_data::*;
-pub use crate::util::validate_results::*;
+pub use crate::util::verify_results::*;
