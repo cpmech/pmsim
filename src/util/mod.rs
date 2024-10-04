8d0dd3077f40e731e83cd76ab95ba545da309da5
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-10-05 08:59:18 +1000

    [wip] Impl elastic_increments_oct

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,11 +1,13 @@
 //! Contains some utility functions and structures
 
 mod convergence_results;
+mod elastic_increments_oct;
 mod paraview;
 mod reference_data;
 mod verify_results;
 
 pub use convergence_results::*;
+pub use elastic_increments_oct::*;
 pub use paraview::*;
 pub use reference_data::*;
 pub use verify_results::*;
