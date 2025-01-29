b8cf568c7738e268ad1a10067a4df0143c5c8906
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-01-29 19:48:37 +1000

    Impl elastic plane-strain pressurized cylinder

--- src/analytical/mod.rs
+++ src/analytical/mod.rs
@@ -1,5 +1,7 @@
 //! This module contains analytical solutions to some problems or reference solutions for testing and verifications
 
+mod elast_plane_strain_press_cylin;
 mod elastic_plane_strain;
 
+pub use elast_plane_strain_press_cylin::*;
 pub use elastic_plane_strain::*;
