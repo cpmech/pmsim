8b559e93aa5ca264ae5d84d36697316d757e8b66
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-01-29 20:57:02 +1000

    [wip] SPO tests

---
+++ tests/test_spo_751_pres_cylin.rs
@@ -0,0 +1,129 @@
+#![allow(unused)]
+
+use gemlab::prelude::*;
+use pmsim::{
+    analytical::ElastPlaneStrainPresCylin,
+    base::SampleMeshes,
+    fem::FileIoSummary,
+    prelude::*,
+    util::{paraview_write_vtu, verify_results},
+};
+use russell_lab::*;
+
+const NAME: &str = "test_spo_751_pres_cylin";
+const GENERATE_MESH: bool = true;
+const SAVE_FIGURE: bool = true;
+
+const R1: f64 = 100.0; // inner radius
+const R2: f64 = 200.0; // outer radius
+const P1: f64 = 0.2; // inner pressure (magnitude)
+const P2: f64 = 0.0; // outer pressure (magnitude)
+const YOUNG: f64 = 210.0; // Young's modulus
+const POISSON: f64 = 0.3; // Poisson's coefficient
+const NGAUSS: usize = 4; // number of gauss points
+
+#[test]
+fn test_spo_751_press_cylin() -> Result<(), StrError> {
+    // mesh
+    let att = 1;
+    let kind = GeoKind::Qua4;
+    let mesh = generate_or_read_mesh(att, kind, GENERATE_MESH);
+
+    // features
+    let features = Features::new(&mesh, false);
+    let bottom = features.search_edges(At::Y(0.0), any_x)?;
+    let left = features.search_edges(At::X(0.0), any_x)?;
+    let inner_circle = features.search_edges(At::Circle(0.0, 0.0, R1), any_x)?;
+
+    // reference point to compare analytical vs numerical result
+    let ref_point_id = features.search_point_ids(At::XY(R1, 0.0), any_x)?[0];
+    array_approx_eq(&mesh.points[ref_point_id].coords, &[R1, 0.0], 1e-15);
+
+    // parameters
+    let param1 = ParamSolid {
+        density: 1.0,
+        stress_strain: StressStrain::LinearElastic {
+            young: YOUNG,
+            poisson: POISSON,
+        },
+    };
+    let fem = FemMesh::new(&mesh, [(1, Elem::Solid(param1))])?;
+
+    // essential boundary conditions
+    let mut essential = Essential::new();
+    essential.edges(&left, Dof::Ux, 0.0).edges(&bottom, Dof::Uy, 0.0);
+
+    // natural boundary conditions
+    let mut natural = Natural::new();
+    natural.edges(&inner_circle, Nbc::Qn, -P1);
+
+    // configuration
+    let mut config = Config::new(&mesh);
+    config.set_ngauss(att, NGAUSS);
+
+    // FEM state
+    let mut state = FemState::new(&fem, &config)?;
+
+    // File IO
+    let mut file_io = FileIo::new(&fem, Some(NAME.to_string()), None)?;
+
+    // solution
+    let mut solver = SolverImplicit::new(&fem, &config, &essential, &natural)?;
+    solver.solve(&mut state, &mut file_io)?;
+
+    // compute error
+    let ana = ElastPlaneStrainPresCylin::new(R1, R2, P1, P2, YOUNG, POISSON)?;
+    let r = mesh.points[ref_point_id].coords[0];
+    assert_eq!(mesh.points[ref_point_id].coords[1], 0.0);
+    let eq = fem.equations.eq(ref_point_id, Dof::Ux).unwrap();
+    let numerical_ur = state.uu[eq];
+    let error = f64::abs(numerical_ur - ana.ur(r));
+    println!("error = {}", error);
+    approx_eq(numerical_ur, ana.ur(r), 1.35e-4);
+
+    Ok(())
+}
+
+/// Generate or read mesh
+fn generate_or_read_mesh(att: usize, kind: GeoKind, generate: bool) -> Mesh {
+    let k_str = kind.to_string();
+
+    if generate {
+        // generate mesh
+        let wr = &[16.0, 20.0, 28.0, 36.0];
+        let na = 3;
+        let mesh = Structured::quarter_ring_2d(R1, R2, wr, na, GeoKind::Qua8, true).unwrap();
+        mesh.check_all().unwrap();
+
+        // draw figure
+        if SAVE_FIGURE {
+            let mut opt = Figure::new();
+            opt.with_point_marker = false;
+            // opt.point_dots = true;
+            opt.point_ids = true;
+            opt.cell_ids = true;
+            opt.with_cell_att = false;
+            opt.figure_size = Some((1000.0, 1000.0));
+            mesh.draw(
+                Some(opt),
+                &format!("{}/mesh_{}_{}.svg", DEFAULT_TEST_DIR, NAME, k_str),
+                |_, _| {},
+            )
+            .unwrap();
+        }
+
+        // write mesh
+        mesh.write_json(&format!("{}/{}_{}.json", DEFAULT_TEST_DIR, NAME, k_str))
+            .unwrap();
+
+        // write VTU
+        mesh.write_vtu(&format!("{}/{}_{}.vtu", DEFAULT_TEST_DIR, NAME, k_str))
+            .unwrap();
+
+        // return mesh
+        mesh
+    } else {
+        // read mesh
+        Mesh::read_json(&format!("data/meshes/{}_{}.json", NAME, k_str)).unwrap()
+    }
+}
--- tests/test_spo_754_footing.rs
+++ tests/test_spo_754_footing.rs
@@ -1,15 +1,16 @@
 #![allow(unused)]
 
 use gemlab::prelude::*;
-use pmsim::{base::SampleMeshes, prelude::*};
+use pmsim::{base::SampleMeshes, fem::FileIoSummary, prelude::*, util::verify_results};
 use russell_lab::*;
 
+const NAME: &str = "test_spo_754_footing";
 const SAVE_FIGURE: bool = true;
 
 #[test]
 fn test_spo_754_footing() -> Result<(), StrError> {
     // mesh
-    let mesh = Mesh::from_text_file("data/meshes/test_spo_754_footing.msh")?;
+    let mesh = Mesh::from_text_file(&format!("data/meshes/{}.msh", NAME))?;
     let att = mesh.cells[0].attribute;
     if SAVE_FIGURE {
         mesh.check_all()?;
@@ -20,7 +21,7 @@ fn test_spo_754_footing() -> Result<(), StrError> {
         // opt.cell_ids = true;
         // opt.figure_size = Some((2000.0, 2000.0));
         opt.figure_size = Some((800.0, 800.0));
-        mesh.draw(Some(opt), "/tmp/pmsim/test_spo_754_footing.svg", |_, _| {})?;
+        mesh.draw(Some(opt), &format!("/tmp/pmsim/{}.svg", NAME), |_, _| {})?;
     }
 
     // features
@@ -43,13 +44,16 @@ fn test_spo_754_footing() -> Result<(), StrError> {
     // parameters
     let p1 = ParamSolid {
         density: 1.0,
-        stress_strain: StressStrain::LinearElastic {
+        stress_strain: StressStrain::VonMises {
             young: 1e+07,
             poisson: 0.48,
+            z_ini: 848.7,
+            hh: 0.0,
         },
     };
     let fem = FemMesh::new(&mesh, [(1, Elem::Solid(p1))])?;
 
+    /*
     const UY: [f64; 15] = [
         0.0,     //  0
         0.0001,  //  1
@@ -67,6 +71,8 @@ fn test_spo_754_footing() -> Result<(), StrError> {
         0.00140, // 13
         0.002,   // 14
     ];
+    */
+    const UY: [f64; 2] = [0.0, 0.0001];
 
     // essential boundary conditions
     let mut essential = Essential::new();
@@ -87,17 +93,26 @@ fn test_spo_754_footing() -> Result<(), StrError> {
         .set_dt(|_| 1.0)
         .set_dt_out(|_| 1.0)
         .set_t_fin((UY.len() - 1) as f64)
+        .set_ignore_jacobian_symmetry(true)
         .set_n_max_iterations(20);
 
     // FEM state
     let mut state = FemState::new(&fem, &config)?;
 
     // File IO
-    let mut file_io = FileIo::new(&fem, None, None)?;
+    let mut file_io = FileIo::new(&fem, Some(NAME.to_string()), None)?;
 
     // solution
     let mut solver = SolverImplicit::new(&fem, &config, &essential, &natural)?;
     solver.solve(&mut state, &mut file_io)?;
+
+    // analysis
+    // let summary = FileIoSummary::read_json(&format!("{}/{}-summary.json", DEFAULT_OUT_DIR, NAME))?;
+
+    // verify the results
+    let tol_displacement = 1e-1;
+    let tol_stress = 1e+3;
+    verify_results(&mesh, NAME, "spo_754_footing.json", tol_displacement, tol_stress, true)?;
 
     Ok(())
 }
