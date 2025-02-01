d5fd489f8e872a19ef354fffb2cf86e62d06ecd8
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-02-01 13:49:49 +1000

    Rename compare_results file

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,11 +1,11 @@
 //! Contains some utility functions and structures
 
+mod compare_results;
 mod convergence_results;
 mod elastic_increments_oct;
 mod reference_data;
-mod verify_results;
 
+pub use compare_results::*;
 pub use convergence_results::*;
 pub use elastic_increments_oct::*;
 pub use reference_data::*;
-pub use verify_results::*;
