8e5c9b6578ce4a57318f4d8efe3e8438bfce45c5
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-02-08 10:21:50 +1000

    Impl solution to spherical shell

--- src/analytical/mod.rs
+++ src/analytical/mod.rs
@@ -3,9 +3,11 @@
 mod elast_plane_strain_flexible_foot;
 mod elast_plane_strain_pres_cylin;
 mod plast_plane_strain_pres_cylin;
+mod plast_plane_strain_pres_sphere;
 mod polar_coordinates;
 
 pub use elast_plane_strain_flexible_foot::*;
 pub use elast_plane_strain_pres_cylin::*;
 pub use plast_plane_strain_pres_cylin::*;
+pub use plast_plane_strain_pres_sphere::*;
 pub use polar_coordinates::*;
