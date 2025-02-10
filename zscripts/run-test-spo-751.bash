375c6bdb6da36d53755cb868b32da5ea9b6a001e
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-02-11 09:41:05 +1000

    Remove scripts to run tests

--- zscripts/run-test-spo-751.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cargo build
-cargo test test_spo_751_press_cylin
-
-~/rust_modules/debug/pmsim_to_paraview /tmp/pmsim/results test_spo_751_pres_cylin
--- zscripts/run-test-spo-755.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cargo build
-cargo test test_spo_755_tensile
-
-~/rust_modules/debug/pmsim_to_paraview /tmp/pmsim/results test_spo_755_tensile
