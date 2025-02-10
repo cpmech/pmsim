e5a6cbe2c680a7871e3432cf13d2fa470c826f0c
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-02-10 19:07:12 +1000

    Rename file reference_data_spo

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,13 +3,13 @@
 mod compare_results;
 mod convergence_results;
 mod elastic_increments_oct;
-mod reference_data;
+mod reference_data_spo;
 mod spatial_tensor;
 mod tensor_components_map;
 
 pub use compare_results::*;
 pub use convergence_results::*;
 pub use elastic_increments_oct::*;
-pub use reference_data::*;
+pub use reference_data_spo::*;
 pub use spatial_tensor::*;
 pub(crate) use tensor_components_map::*;
