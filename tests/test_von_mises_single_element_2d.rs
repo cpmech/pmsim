b4dd94020692b3231cc24f0125b4671d11a40e6c
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-02-26 15:43:26 +1000

    Remove unused comment in test

--- tests/test_von_mises_single_element_2d.rs
+++ tests/test_von_mises_single_element_2d.rs
@@ -101,7 +101,7 @@ fn test_von_mises_single_element_2d() -> Result<(), StrError> {
     // solve and check with UMFPACK
     let mut config = Config::new(&mesh);
     config
-        // .set_lin_sol_genie(Genie::Mumps)
+        .set_richardson_extrapolation(false)
         .set_lagrange_mult_method(true)
         .set_incremental(N_STATION)
         .set_n_max_iterations(20);
