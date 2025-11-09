0233b921b6bebb07ec7f1f4db5606d577b5e6ccd
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-11-09 16:36:55 +1000

    [wip] Debug L-shape flow problem

--- tests/test_seep_craig_2d9.rs
+++ tests/test_seep_craig_2d9.rs
@@ -1,7 +1,11 @@
+#![allow(unused)]
+
 use gemlab::graph::GraphUnd;
 use gemlab::prelude::*;
+use plotpy::Plot;
 use pmsim::prelude::*;
 use pmsim::StrError;
+use tritet::Trigen;
 
 const OUT_DIR: &str = "/tmp/pmsim";
 const NAME: &str = "test_seep_craig_2d9";
@@ -33,16 +37,21 @@ const NYP_FINER: usize = 4; // number of divisions for the pile
 #[test]
 fn test_seep_craig_2d9() -> Result<(), StrError> {
     // mesh
-    let coarse = true;
-    let mesh = generate_or_read_mesh(GENERATE_MESH, coarse);
+    let finer = true;
+    let o2 = true;
+    // let mesh = generate_or_read_mesh(GENERATE_MESH, finer, o2);
+    // let mesh = generate_or_read_mesh_simple();
+    let mesh = generate_or_read_mesh_simple_tri();
 
     // features
     let features = Features::new(&mesh, false);
-    let left_top = features.search_edges(At::Y(L), |x| x[0] <= M)?;
-    let right_top = features.search_edges(At::Y(R), |x| x[0] >= M + T)?;
+    // let left_top = features.search_edges(At::Y(L), |x| x[0] <= M)?;
+    // let right_top = features.search_edges(At::Y(R), |x| x[0] >= M + T)?;
+    let inlet = features.search_edges(At::Y(2.0), |_| true)?;
+    let outlet = features.search_edges(At::X(2.0), |_| true)?;
 
     // parameters
-    let (kx, ky) = (2.6e-5, 2.6e-5);
+    let (kx, ky) = (1.0, 1.0);
     let p1 = ParamDiffusion {
         rho: 1.0,
         conductivity: Conductivity::Constant { kx, ky, kz: 0.0 },
@@ -53,8 +62,8 @@ fn test_seep_craig_2d9() -> Result<(), StrError> {
 
     // essential boundary conditions
     let mut essential = Essential::new();
-    essential.edges(&left_top, Dof::Phi, 25.0);
-    essential.edges(&right_top, Dof::Phi, 0.0);
+    essential.edges(&inlet, Dof::Phi, 25.0);
+    essential.edges(&outlet, Dof::Phi, 0.0);
 
     // natural boundary conditions
     let natural = Natural::new();
@@ -90,6 +99,8 @@ fn post_processing() -> Result<(), StrError> {
     let mesh = post.mesh();
     let with_internal_edges = true;
     let features = Features::new(&mesh, with_internal_edges); // need internal edges
+
+    /*
     let gap_section = features.search_edges(At::X(M), |x| x[1] <= G)?;
     let cells_around_gap_section = features.get_cells_via_2d_edges(&gap_section);
     let gap_cells: Vec<_> = cells_around_gap_section
@@ -106,12 +117,13 @@ fn post_processing() -> Result<(), StrError> {
         .collect();
     println!("section = {}", gap_section);
     println!("gap_cells = {:?}", gap_cells);
+    */
 
     // analysis
     let state = post.read_state(post.n_state() - 1)?;
-    let gauss = post.gauss_fluxes_patch(&mut memo, &state, &gap_cells, Dof::Phi, |_, _, _| true)?;
-    println!("wx = {:?}", gauss.vvx);
-    println!("wy = {:?}", gauss.vvy);
+    // let gauss = post.gauss_fluxes_patch(&mut memo, &state, &gap_cells, Dof::Phi, |_, _, _| true)?;
+    // println!("wx = {:?}", gauss.vvx);
+    // println!("wy = {:?}", gauss.vvy);
 
     // write Paraview files
     let path_pvd = post.write_paraview(&mut memo, OUT_DIR, NAME)?;
@@ -122,9 +134,9 @@ fn post_processing() -> Result<(), StrError> {
 }
 
 /// Generate or read mesh
-fn generate_or_read_mesh(generate: bool, coarse: bool) -> Mesh {
+fn generate_or_read_mesh(generate: bool, finer: bool, o2: bool) -> Mesh {
     if generate {
-        let kind = GeoKind::Qua4;
+        let kind = if o2 { GeoKind::Qua8 } else { GeoKind::Qua4 };
 
         // set blocks on the left-hand side
         let mut block1 = Block::new(&[[0.0, 0.0], [M, 0.0], [M, G], [0.0, G]]).unwrap();
@@ -138,21 +150,21 @@ fn generate_or_read_mesh(generate: bool, coarse: bool) -> Mesh {
         let mut block5 = Block::new(&[[M + T, 0.0], [M + T + N, 0.0], [M + T + N, G], [M + T, G]]).unwrap();
         let mut block6 = Block::new(&[[M + T, G], [M + T + N, G], [M + T + N, R], [M + T, R]]).unwrap();
 
-        // set number of division (finer)
-        if coarse {
-            block1.set_ndiv(&[NYM_COARSE, NYG_COARSE]).unwrap();
-            block2.set_ndiv(&[NYM_COARSE, NYP_COARSE]).unwrap();
-            block3.set_ndiv(&[NYM_COARSE, NYL_COARSE]).unwrap();
-            block4.set_ndiv(&[1, NYG_COARSE]).unwrap();
-            block5.set_ndiv(&[NYN_COARSE, NYG_COARSE]).unwrap();
-            block6.set_ndiv(&[NYN_COARSE, NYP_COARSE]).unwrap();
-        } else {
+        // set number of division
+        if finer {
             block1.set_ndiv(&[NYM_FINER, NYG_FINER]).unwrap();
             block2.set_ndiv(&[NYM_FINER, NYP_FINER]).unwrap();
             block3.set_ndiv(&[NYM_FINER, NYL_FINER]).unwrap();
             block4.set_ndiv(&[1, NYG_FINER]).unwrap();
             block5.set_ndiv(&[NYN_FINER, NYG_FINER]).unwrap();
             block6.set_ndiv(&[NYN_FINER, NYP_FINER]).unwrap();
+        } else {
+            block1.set_ndiv(&[NYM_COARSE, NYG_COARSE]).unwrap();
+            block2.set_ndiv(&[NYM_COARSE, NYP_COARSE]).unwrap();
+            block3.set_ndiv(&[NYM_COARSE, NYL_COARSE]).unwrap();
+            block4.set_ndiv(&[1, NYG_COARSE]).unwrap();
+            block5.set_ndiv(&[NYN_COARSE, NYG_COARSE]).unwrap();
+            block6.set_ndiv(&[NYN_COARSE, NYP_COARSE]).unwrap();
         }
 
         // generate mesh
@@ -182,4 +194,65 @@ fn generate_or_read_mesh(generate: bool, coarse: bool) -> Mesh {
         // read mesh
         Mesh::read(&format!("data/meshes/{}.msh", NAME)).unwrap()
     }
+}
+
+fn generate_or_read_mesh_simple() -> Mesh {
+    let mut block1 = Block::new(&[[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]]).unwrap();
+    let mut block2 = Block::new(&[[0.0, 1.0], [1.0, 1.0], [1.0, 2.0], [0.0, 2.0]]).unwrap();
+    let mut block3 = Block::new(&[[1.0, 0.0], [2.0, 0.0], [2.0, 1.0], [1.0, 1.0]]).unwrap();
+    block1.set_ndiv(&[4, 4]).unwrap();
+    block2.set_ndiv(&[4, 4]).unwrap();
+    block3.set_ndiv(&[4, 4]).unwrap();
+    let mesh1 = block1.subdivide(GeoKind::Qua4).unwrap();
+    let mesh2 = block2.subdivide(GeoKind::Qua4).unwrap();
+    let mesh3 = block3.subdivide(GeoKind::Qua4).unwrap();
+    let mesh = join_meshes(&[&mesh1, &mesh2, &mesh3]).unwrap();
+    mesh.check_all().unwrap();
+    Figure::new()
+        .show_point_ids(true)
+        .show_cell_ids(true)
+        .show_cell_att(false)
+        .size(800.0, 800.0)
+        .draw(&mesh, &format!("{}/mesh_{}.svg", OUT_DIR, NAME))
+        .unwrap();
+    mesh
+}
+
+fn generate_or_read_mesh_simple_tri() -> Mesh {
+    let mut gen = Trigen::new(6, Some(6), Some(1), None).unwrap();
+    gen.set_point(0, 0, 0.0, 0.0).unwrap();
+    gen.set_point(1, 0, 2.0, 0.0).unwrap();
+    gen.set_point(2, 0, 2.0, 1.0).unwrap();
+    gen.set_point(3, 0, 1.0, 1.0).unwrap();
+    gen.set_point(4, 0, 1.0, 2.0).unwrap();
+    gen.set_point(5, 0, 0.0, 2.0).unwrap();
+    gen.set_segment(0, 0, 0, 1).unwrap();
+    gen.set_segment(1, 1, 1, 2).unwrap();
+    gen.set_segment(2, 0, 2, 3).unwrap();
+    gen.set_segment(3, 0, 3, 4).unwrap();
+    gen.set_segment(4, 4, 4, 5).unwrap();
+    gen.set_segment(5, 0, 5, 0).unwrap();
+    gen.set_region(0, 1, 0.1, 0.1, None).unwrap();
+    gen.generate_mesh(false, false, true, Some(0.01), None).unwrap();
+
+    let mut plot = Plot::new();
+    // gen.draw_triangles(&mut plot, true, false, false, false, None, None, None);
+    plot.set_figure_size_points(800.0, 800.0)
+        .save(&format!("{}/mesh_{}_tri.svg", OUT_DIR, NAME))
+        .unwrap();
+
+    let mesh = Unstructured::from_trigen(&gen);
+    mesh.check_all().unwrap();
+
+    /*
+    Figure::new()
+        .show_point_ids(true)
+        .show_cell_ids(true)
+        .show_cell_att(false)
+        .size(800.0, 800.0)
+        .draw(&mesh, &format!("{}/mesh_{}.svg", OUT_DIR, NAME))
+        .unwrap();
+    */
+
+    mesh
 }
