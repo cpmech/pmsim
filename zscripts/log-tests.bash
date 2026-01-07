9874344d7a3ec8bea52bba5bd7be75032bd0623f
Dorival Pedroso <dorival.pedroso@gmail.com>
2026-01-07 10:41:33 +1000

    Rename log (output) files

--- zscripts/log-tests.bash
+++ zscripts/log-tests.bash
@@ -2,6 +2,8 @@
 
 TESTS=(
     test_spo_751_pres_cylin
+    test_spo_752_pres_sphere
+    test_spo_753_circ_plate
     test_spo_754_footing
     test_spo_755_tensile
     test_spo_von_mises_2x2_elements
@@ -11,5 +13,6 @@ TESTS=(
 
 mkdir -p /tmp/pmsim
 for test in "${TESTS[@]}"; do
-    cargo test --test "$test" -- --nocapture > "/tmp/pmsim/log-${test}.txt"
+    cargo test --test "$test" -- --nocapture > "/tmp/pmsim/out-${test}.txt"
+    # mv "/tmp/pmsim/out-${test}.txt" "data/logs/${test}.txt"
 done
