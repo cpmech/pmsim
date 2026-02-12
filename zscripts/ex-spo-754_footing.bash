cd25f5401a02dd38a8a615d2da9327e23dad038e
Dorival Pedroso <dorival.pedroso@gmail.com>
2026-02-12 16:32:44 +1000

    Run more tests in SPO 754 examples script

--- zscripts/ex-spo-754_footing.bash
+++ zscripts/ex-spo-754_footing.bash
@@ -8,10 +8,17 @@ cargo run --release --example spo_754_footing -- -g mumps
 cargo run --release --example spo_754_footing -- -g mumps --lmm
 cargo run --release --example spo_754_footing -- -g mumps --arclength
 cargo run --release --example spo_754_footing -- -g mumps --arclength --lmm
+cargo run --release --example spo_754_footing -- -g mumps --bordering
+cargo run --release --example spo_754_footing -- -g mumps --lmm --bordering
+cargo run --release --example spo_754_footing -- -g mumps --arclength --bordering
+cargo run --release --example spo_754_footing -- -g mumps --arclength --lmm --bordering
 
 cargo run --release --example spo_754_footing -- -g klu --arclength
 cargo run --release --example spo_754_footing -- -g umfpack --arclength
 cargo run --release --example spo_754_footing -- -g umfpack --arclength --lmm
+cargo run --release --example spo_754_footing -- -g klu --arclength --bordering
+cargo run --release --example spo_754_footing -- -g umfpack --arclength --bordering
+cargo run --release --example spo_754_footing -- -g umfpack --arclength --lmm --bordering
 
 echo
 echo
